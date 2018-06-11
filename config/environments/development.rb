4e634404ed88cc52539d7b62ea90c58525fae23f
Chet Swalina <cswalina@gmail.com>
2018-06-11 17:07:52 -0400

    Mailer hostnames for development and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'psu-research-metadata.dev' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
