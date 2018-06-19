a6da4a3d52d9ef319e4be3336744c9966a8e8c8b
Chet Swalina <cswalina@gmail.com>
2018-06-18 23:04:29 -0400

    Avoid rails admin dependency on active storage tables
    Out-of-the-box rails_admin will throw a table not found error
    because it's expecting active storage tables to be present
    by default. See:  https://github.com/sferik/rails_admin/issues/3014

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -23,6 +23,8 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
+  config.excluded_models = ['ActiveStorage::Blob', 'ActiveStorage::Attachment']
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
