4d3f85e6290438f2929fada1ea141276232f06b0
Chet Swalina <cswalina@gmail.com>
2018-06-25 15:57:20 -0400

    Generate Swagger API docs around publications
    In rails console:
    1)  SWAGGERED_CLASSES = [API::V1::PublicationsController, ApidocsController, Publication, ErrorModel]
    2)  swagger_data = Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
    3)  File.open('<YOUR PATH HERE>/psu-research-metadata/public/api_docs/swagger_docs/v1/swagger.json', 'w') { |file| file.write(swagger_data.to_json) }

--- app/controllers/api/v1/publications_controller.rb
+++ app/controllers/api/v1/publications_controller.rb
@@ -1,5 +1,7 @@
 module API::V1
   class PublicationsController < APIController
+    include Swagger::Blocks
+
     def index
       render json: API::V1::PublicationSerializer.new(Publication.all).serialized_json
     end
@@ -7,6 +9,67 @@ module API::V1
     def show
       @publication = Publication.find(params[:id])
       render json: API::V1::PublicationSerializer.new(@publication).serializable_hash
+    end
+
+    swagger_path '/publications/{id}' do
+      operation :get do
+        key :summary, 'Find Publication by ID'
+        key :description, 'Returns a single publication if the user has access'
+        key :operationId, 'findPublicationById'
+        key :tags, [
+          'publication'
+        ]
+        parameter do
+          key :name, :id
+          key :in, :path
+          key :description, 'ID of publication to fetch'
+          key :required, true
+          key :type, :integer
+          key :format, :int64
+        end
+        response 200 do
+          key :description, 'publication response'
+          schema do
+            key :'$ref', :Publication
+          end
+        end
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorModel
+          end
+        end
+      end
+    end
+
+    swagger_path '/publications' do
+      operation :get do
+        key :summary, 'All Publications'
+        key :description, 'Returns all publications from the system that the user has access to'
+        key :operationId, 'findPublications'
+        key :produces, [
+          'application/json',
+          'text/html',
+        ]
+        key :tags, [
+          'publications'
+        ]
+        response 200 do
+          key :description, 'publication response'
+          schema do
+            key :type, :array
+            items do
+              key :'$ref', :Publication
+            end
+          end
+        end
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorModel
+          end
+        end
+      end
     end
   end
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,31 @@
 class Publication < ActiveRecord::Base
+  include Swagger::Blocks
+
   validates :title, presence: true
+
+  swagger_schema :Publication do
+    key :required, [:id, :title]
+    property :id do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :title do
+      key :type, :string
+    end
+  end
+
+  swagger_schema :PublicationInput do
+    allOf do
+      schema do
+        key :'$ref', :Publication
+      end
+      schema do
+        key :required, [:title]
+        property :id do
+          key :type, :integer
+          key :format, :int64
+        end
+      end
+    end
+  end
 end
