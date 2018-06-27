b9285ed0c9edc67be19f19686e04f36ebaa4ca8e
Eric Durante <eric@westarete.com>
2018-06-27 16:45:47 -0400

    Hook this app up to a new Bugsnag project

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = "38dba23a048384feed73aedadbdf17d2"
+  config.api_key = "9decc74da9fb88743e79ee58bdc8a130"
   config.notify_release_stages = %w{ staging beta production }
 end
