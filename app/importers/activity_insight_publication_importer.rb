957805d0d8e2ff70af55637790db55e861382336
Chet Swalina <cswalina@gmail.com>
2018-07-03 09:17:26 -0400

    Add more attributes to publication importer

--- app/importers/activity_insight_publication_importer.rb
+++ app/importers/activity_insight_publication_importer.rb
@@ -57,6 +57,25 @@ class ActivityInsightPublicationImporter < CSVImporter
 
       p = Publication.new
       p.title = row[:title]
+      p.activity_insight_identifier = row[:id]
+      p.characteristic = "Journal Article, Academic Journal"
+      p.secondary_title = row[:title_secondary]
+      p.source = row[:journal_name]
+      p.issue = row[:issue]
+      p.edition = row[:edition]
+      p.isbn_issn = row[:isbnissn]
+      p.abstract = row[:abstract]
+
+      p.source = extract_value(row: row, header_key: :journal_name, header_count: 3)
+      p.source = row[:journal_name_other] if row[:journal_name_other].present?
+      p.status = extract_value(row: row, header_key: :status, header_count: 3)
+      p.volume = extract_value(row: row, header_key: :volume, header_count: 2)
+      p.page_range = extract_value(row: row, header_key: :pagenum, header_count: 4)
+      unless p.page_range.present?
+        p.page_range = extract_value(row: row, header_key: :pub_pagenum, header_count: 2)
+      end
+      p.url = extract_value(row: row, header_key: :web_address, header_count: 3)
+      p.published_at = extract_value(row: row, header_key: :pub_start, header_count: 5)
       p
     else
       nil
@@ -71,5 +90,18 @@ class ActivityInsightPublicationImporter < CSVImporter
 
   def encoding
     'bom|utf-8'
+  end
+
+  def extract_value(row:, header_key:, header_count:)
+    value = ''
+    header_count.times do |i|
+      if i == 0
+        value = row[header_key] if row[header_key].present?
+      else
+        key = header_key.to_s + (i+1).to_s
+        value = row[key.to_sym] if row[key.to_sym].present?
+      end
+    end
+    value
   end
 end
