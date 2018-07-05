eaebb8b2bdf6f04240cde9001a96749c46816581
Eric Durante <eric@westarete.com>
2018-07-05 13:49:00 -0400

    Update integration spec to match new page content

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'integration/integration_spec_helper'
 feature "Home page", type: :feature do
   scenario "visit" do
     visit root_path
-    expect(page).to have_content "Research Metadata Test"
+    expect(page).to have_content "PSU Libraries Research Metadata"
   end
 end
