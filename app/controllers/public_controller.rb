42fbf82d7f1e89c84dba2bbd68c6a8aca9746537
Eric Durante <eric@westarete.com>
2018-07-06 11:24:32 -0400

    Authenticate users visiting the home page
    Ultimately, all actions in this application will require a user to
    authenticate in some way (except for reading the API documentation
    possibly).

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,6 @@
 class PublicController < ApplicationController
+  before_action :authenticate_user!
+
   def home
     @env = Rails.env
   end
--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -1,8 +1,14 @@
 require 'integration/integration_spec_helper'
 
 feature "Home page", type: :feature do
-  scenario "visit" do
-    visit root_path
-    expect(page).to have_content "PSU Libraries Research Metadata"
+  let(:home_content) { "PSU Libraries Research Metadata" }
+
+  context "when the user is logged in" do
+    before { authenticate_user }
+
+    it "shows the home page content" do
+      visit root_path
+      expect(page).to have_content home_content
+    end
   end
 end
