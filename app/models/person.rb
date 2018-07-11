a794a04e32203c1bebe1bf86a0315fe035258300
Chet Swalina <cswalina@gmail.com>
2018-07-11 15:23:28 -0400

    Customize the admin person show page ...
    A first pass at customizing rails_admin views.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,23 @@ class Person < ApplicationRecord
   has_many :authorships
   has_many :publications, through: :authorships
   validates :first_name, :last_name, presence: true
+
+  def name
+    full_name = first_name
+    full_name = full_name + ' ' + middle_name if middle_name
+    full_name = full_name + ' ' + last_name if last_name
+    full_name
+  end
+
+  rails_admin do
+    configure :publications do
+      pretty_value do
+        bindings[:view].render :partial => "rails_admin/partials/users/publications.html.erb", :locals => { :publications => value }
+      end
+    end
+
+    show do
+      field :publications
+    end
+  end
 end
