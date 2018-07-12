fcc4e0247fa8cbd22046a5d9d0d87b40419b15d6
Eric Durante <eric@westarete.com>
2018-07-12 10:54:09 -0400

    Make the Activity Insight user importer idempotent
    As we’re instantiating new users from rows in the CSV file, we need to
    check to see if a user with the same WebAccess ID already exists in the
    database. We also need to check whether or not there is more than one
    row with the same access ID within a given batch since the batches are
    bulk-inserted and model validations are bypassed.

--- app/importers/activity_insight_user_importer.rb
+++ app/importers/activity_insight_user_importer.rb
@@ -1,25 +1,31 @@
 class ActivityInsightUserImporter < CSVImporter
 
   def row_to_object(row)
-    u = User.new
-    u.first_name = row[:first_name]
-    u.middle_name = row[:middle_name]
-    u.last_name = row[:last_name]
-    u.institution = 'Penn State University'
-    u.webaccess_id = row[:username]
-#   u.email = row[:email]
-#   u.campus_abbreviation = row[:campus]
-#   u.campus_name = row[:campus_name]
-#   u.college_abbreviation = row[:college]
-#   u.college_name = row[:college_name]
-#   u.department = row[:department]
-#   u.division = row[:division]
-#   u.institute = row[:institute]
-#   u.school = row[:school]
-    u
+    unless User.find_by(webaccess_id: row[:username])
+      u = User.new
+      u.first_name = row[:first_name]
+      u.middle_name = row[:middle_name]
+      u.last_name = row[:last_name]
+      u.institution = 'Penn State University'
+      u.webaccess_id = row[:username]
+  #   u.email = row[:email]
+  #   u.campus_abbreviation = row[:campus]
+  #   u.campus_name = row[:campus_name]
+  #   u.college_abbreviation = row[:college]
+  #   u.college_name = row[:college_name]
+  #   u.department = row[:department]
+  #   u.division = row[:division]
+  #   u.institute = row[:institute]
+  #   u.school = row[:school]
+      u
+    end
   end
 
   def bulk_import(objects)
-    User.import(objects)
+    unique_users = objects.uniq { |o| o.webaccess_id }
+    if objects.count != unique_users.count
+      fatal_errors << "The file contains at least one duplicate user."
+    end
+    User.import(unique_users)
   end
 end
