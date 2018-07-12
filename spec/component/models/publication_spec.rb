bad88ddc64be9ccee44c5477ac98b83bc4692e6a
Eric Durante <eric@westarete.com>
2018-07-12 15:19:19 -0400

    Reformat Publication model spec

--- spec/component/models/publication_spec.rb
+++ spec/component/models/publication_spec.rb
@@ -1,30 +1,30 @@
 require 'component/component_spec_helper'
 
-RSpec.describe Publication, type: :model do
-  describe 'the publications table' do
-    subject { Publication.new }
-    it { is_expected.to have_db_column(:title).of_type(:text).with_options(null: false) }
-    it { is_expected.to have_db_column(:publication_type).of_type(:string).with_options(null: false) }
-    it { is_expected.to have_db_column(:journal_title).of_type(:text) }
-    it { is_expected.to have_db_column(:publisher).of_type(:text) }
-    it { is_expected.to have_db_column(:secondary_title).of_type(:text) }
-    it { is_expected.to have_db_column(:status).of_type(:string) }
-    it { is_expected.to have_db_column(:volume).of_type(:string) }
-    it { is_expected.to have_db_column(:issue).of_type(:string) }
-    it { is_expected.to have_db_column(:edition).of_type(:string) }
-    it { is_expected.to have_db_column(:page_range).of_type(:string) }
-    it { is_expected.to have_db_column(:url).of_type(:text) }
-    it { is_expected.to have_db_column(:isbn).of_type(:string) }
-    it { is_expected.to have_db_column(:issn).of_type(:string) }
-    it { is_expected.to have_db_column(:doi).of_type(:string) }
-    it { is_expected.to have_db_column(:abstract).of_type(:text) }
-    it { is_expected.to have_db_column(:outside_contributors).of_type(:text) }
-    it { is_expected.to have_db_column(:authors_et_al).of_type(:boolean) }
-    it { is_expected.to have_db_column(:published_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
-    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
-  end
+describe 'the publications table', type: :model do
+  subject { Publication.new }
+
+  it { is_expected.to have_db_column(:title).of_type(:text).with_options(null: false) }
+  it { is_expected.to have_db_column(:publication_type).of_type(:string).with_options(null: false) }
+  it { is_expected.to have_db_column(:journal_title).of_type(:text) }
+  it { is_expected.to have_db_column(:publisher).of_type(:text) }
+  it { is_expected.to have_db_column(:secondary_title).of_type(:text) }
+  it { is_expected.to have_db_column(:status).of_type(:string) }
+  it { is_expected.to have_db_column(:volume).of_type(:string) }
+  it { is_expected.to have_db_column(:issue).of_type(:string) }
+  it { is_expected.to have_db_column(:edition).of_type(:string) }
+  it { is_expected.to have_db_column(:page_range).of_type(:string) }
+  it { is_expected.to have_db_column(:url).of_type(:text) }
+  it { is_expected.to have_db_column(:isbn).of_type(:string) }
+  it { is_expected.to have_db_column(:issn).of_type(:string) }
+  it { is_expected.to have_db_column(:doi).of_type(:string) }
+  it { is_expected.to have_db_column(:abstract).of_type(:text) }
+  it { is_expected.to have_db_column(:outside_contributors).of_type(:text) }
+  it { is_expected.to have_db_column(:authors_et_al).of_type(:boolean) }
+  it { is_expected.to have_db_column(:published_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
 end
+
 
 describe Publication, type: :model do
   describe 'validations' do
