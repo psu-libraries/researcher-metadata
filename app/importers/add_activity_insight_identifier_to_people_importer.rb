5df46fefcfa6cc2e5a90e0c01d80f5861d7810ff
Eric Durante <eric@westarete.com>
2018-07-12 14:46:12 -0400

    Find users by lowercase WebAccess ID
    We’re always storing the WebAccess ID as lowercase now.

--- app/importers/add_activity_insight_identifier_to_people_importer.rb
+++ app/importers/add_activity_insight_identifier_to_people_importer.rb
@@ -43,7 +43,7 @@ class AddActivityInsightIdentifierToPeopleImporter < CSVImporter
 
 
   def row_to_object(row)
-    webaccess_id = row[:username]
+    webaccess_id = row[:username].downcase
     activity_insight_identifier = row[:user_id]
     user = User.find_by(webaccess_id: webaccess_id)
     user.update_column(:activity_insight_identifier, activity_insight_identifier) if user
