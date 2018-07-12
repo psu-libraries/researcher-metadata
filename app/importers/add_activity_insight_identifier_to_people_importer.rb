7375b24da24849df8aa1f846e7307e189e45f83c
Eric Durante <eric@westarete.com>
2018-07-12 14:03:48 -0400

    Don’t try to import Activity Insight identifier if no user is found

--- app/importers/add_activity_insight_identifier_to_people_importer.rb
+++ app/importers/add_activity_insight_identifier_to_people_importer.rb
@@ -46,7 +46,7 @@ class AddActivityInsightIdentifierToPeopleImporter < CSVImporter
     webaccess_id = row[:username]
     activity_insight_identifier = row[:user_id]
     user = User.find_by(webaccess_id: webaccess_id)
-    user.update_column(:activity_insight_identifier, activity_insight_identifier)
+    user.update_column(:activity_insight_identifier, activity_insight_identifier) if user
     nil
   end
 
