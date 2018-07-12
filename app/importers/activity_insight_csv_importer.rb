05f4430c85e56b9c6ac97a6e3176cac53b86bbe4
Eric Durante <eric@westarete.com>
2018-07-12 15:01:05 -0400

    Move duplicated code up into superclass

---
+++ app/importers/activity_insight_csv_importer.rb
@@ -0,0 +1,42 @@
+class ActivityInsightCSVImporter < CSVImporter
+  def call
+    tf = Tempfile.new("temp_#{self.class.to_s.underscore}")
+
+    File.foreach(filename).with_index do |line, i|
+      if i == 0
+        headers = line.strip.gsub('"', '').split(',')
+        header_counts = {}
+
+        headers.each do |header|
+          if header_counts.has_key?(header)
+            header_counts[header] += 1
+          else
+            header_counts[header] = 1
+          end
+        end
+
+        new_headers = []
+
+        headers.reverse.each do |header|
+          if header_counts[header] == 1
+            new_headers.push(header)
+          else
+            new_headers.push(header + header_counts[header].to_s)
+            header_counts[header] -= 1
+          end
+        end
+
+        new_header_row = new_headers.reverse.map { |h| "\"#{h}\"" }.join(',')
+        tf.puts new_header_row
+      else
+        tf.puts line
+      end
+    end
+
+    self.filename = tf.path
+
+    super
+
+    tf.unlink
+  end
+end
--- app/importers/add_activity_insight_identifier_to_people_importer.rb
+++ app/importers/add_activity_insight_identifier_to_people_importer.rb
@@ -1,47 +1,4 @@
-class AddActivityInsightIdentifierToPeopleImporter < CSVImporter
-
-  def call
-    tf = Tempfile.new('temp_publications')
-
-    File.foreach(filename).with_index do |line, i|
-      if i == 0
-        headers = line.strip.gsub('"', '').split(',')
-        header_counts = {}
-
-        headers.each do |header|
-          if header_counts.has_key?(header)
-            header_counts[header] += 1
-          else
-            header_counts[header] = 1
-          end
-        end
-
-        new_headers = []
-
-        headers.reverse.each do |header|
-          if header_counts[header] == 1
-            new_headers.push(header)
-          else
-            new_headers.push(header + header_counts[header].to_s)
-            header_counts[header] -= 1
-          end
-        end
-
-        new_header_row = new_headers.reverse.map { |h| "\"#{h}\"" }.join(',')
-        tf.puts new_header_row
-      else
-        tf.puts line
-      end
-    end
-
-    self.filename = tf.path
-
-    super
-
-    tf.unlink
-  end
-
-
+class AddActivityInsightIdentifierToPeopleImporter < ActivityInsightCSVImporter
   def row_to_object(row)
     webaccess_id = row[:username].downcase
     activity_insight_identifier = row[:user_id]
