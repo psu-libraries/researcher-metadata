278ea4a5ccceb36cbd8a76fdafd531f48e7d2e58
Eric Durante <eric@westarete.com>
2018-07-12 17:50:05 -0400

    Make Activity Insight Authorship importer compatible with new schema
    We still need to import “outside contributor” data for publications from
    this same file, but that might involve a completely separate importer.

--- app/importers/activity_insight_authorship_importer.rb
+++ app/importers/activity_insight_authorship_importer.rb
@@ -2,19 +2,13 @@ class ActivityInsightAuthorshipImporter < ActivityInsightCSVImporter
   def row_to_object(row)
     if row[:faculty_name].present?
       authorship = Authorship.new
-      authorship.user = User.find_by(activity_insight_identifier: row[:user_id])
+      authorship.user = User.find_by(activity_insight_identifier: row[:faculty_name])
+      authorship.publication = PublicationImport.find_by(import_source: "Activity Insight",
+                                                         source_identifier: row[:parent_id]).publication
+      authorship.author_number = row[:ordinal].to_i
+      authorship.activity_insight_identifier = row[:id]
+      authorship
     end
-    authorship = Authorship.new
-
-    if row[:faculty_name].present?
-      authorship.person = Person.find_by(activity_insight_identifier: row[:user_id])
-    else
-      authorship.person = Person.create!(first_name: row[:fname], middle_name: row[:mname], last_name: row[:lname])
-    end
-    authorship.publication = Publication.find_by(activity_insight_identifier: row[:parent_id])
-    authorship.author_number = row[:ordinal].to_i
-    authorship.activity_insight_identifier = row[:id]
-    authorship
   end
 
   def bulk_import(objects)
