99c7150da9808f07d418b40af22b5060d872b130
Eric Durante <eric@westarete.com>
2018-07-12 17:11:30 -0400

    Reformat Authorship model spec

--- spec/component/models/authorship_spec.rb
+++ spec/component/models/authorship_spec.rb
@@ -1,22 +1,23 @@
 require 'component/component_spec_helper'
 
-RSpec.describe Authorship, type: :model do
-  describe 'the authorships table' do
-    subject { Authorship.new }
-    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
-    it { is_expected.to have_db_column(:publication_id).of_type(:integer) }
-    it { is_expected.to have_db_column(:author_number).of_type(:integer) }
-    it { is_expected.to have_db_column(:activity_insight_identifier).of_type(:string) }
-    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+describe 'the authorships table', type: :model do
+  subject { Authorship.new }
 
-    it { is_expected.to have_db_index :user_id }
-    it { is_expected.to have_db_index :publication_id }
+  it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+  it { is_expected.to have_db_column(:publication_id).of_type(:integer) }
+  it { is_expected.to have_db_column(:author_number).of_type(:integer) }
+  it { is_expected.to have_db_column(:activity_insight_identifier).of_type(:string) }
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 
-    it { is_expected.to have_db_foreign_key :user_id }
-    it { is_expected.to have_db_foreign_key :publication_id }
-  end
+  it { is_expected.to have_db_index :user_id }
+  it { is_expected.to have_db_index :publication_id }
 
+  it { is_expected.to have_db_foreign_key :user_id }
+  it { is_expected.to have_db_foreign_key :publication_id }
+end
+
+describe Authorship, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:publication) }
