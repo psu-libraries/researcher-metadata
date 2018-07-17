6a969d24be7f75eeb8f711a6de23921c6ac7518f
Eric Durante <eric@westarete.com>
2018-07-17 12:07:23 -0400

    Fix factory value to work with case-sensitive uniqueness validation

--- spec/factories/publication_import.rb
+++ spec/factories/publication_import.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     publication { create :publication }
     title { 'Test' }
     publication_type { 'Academic Journal Article' }
-    sequence(:source_identifier) { |n| n }
+    sequence(:source_identifier) { |n| "abc#{n}" }
     import_source { 'Activity Insight' }
   end
 end
