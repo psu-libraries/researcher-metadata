e31c472bc36ac18cfdef779ac6b032c05c5ff2b2
Eric Durante <eric@westarete.com>
2018-07-19 17:27:49 -0400

    Make Activity Insight authorship importing idempotent

--- app/importers/activity_insight_authorship_importer.rb
+++ app/importers/activity_insight_authorship_importer.rb
@@ -1,13 +1,17 @@
 class ActivityInsightAuthorshipImporter < ActivityInsightCSVImporter
   def row_to_object(row)
     if row[:faculty_name].present?
-      authorship = Authorship.new
-      authorship.user = User.find_by(activity_insight_identifier: row[:faculty_name])
-      authorship.publication = PublicationImport.find_by(import_source: "Activity Insight",
-                                                         source_identifier: row[:parent_id]).publication
-      authorship.author_number = row[:ordinal].to_i
-      authorship.activity_insight_identifier = row[:id]
-      authorship
+      if ! Authorship.find_by(activity_insight_identifier: row[:id])
+        authorship = Authorship.new
+        authorship.user = User.find_by(activity_insight_identifier: row[:faculty_name])
+        authorship.publication = PublicationImport.find_by(import_source: "Activity Insight",
+                                                           source_identifier: row[:parent_id]).publication
+        authorship.author_number = row[:ordinal].to_i
+        authorship.activity_insight_identifier = row[:id]
+        authorship
+      else
+        nil
+      end
     end
   end
 
