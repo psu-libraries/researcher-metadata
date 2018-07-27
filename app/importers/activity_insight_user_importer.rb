21cb23a8573389d7672cdb45bd0d32003f25d463
Eric Durante <eric@westarete.com>
2018-07-27 14:13:45 -0400

    Import WebAccess IDs from Activity Insight as lowercase

--- app/importers/activity_insight_user_importer.rb
+++ app/importers/activity_insight_user_importer.rb
@@ -1,13 +1,15 @@
 class ActivityInsightUserImporter < CSVImporter
 
   def row_to_object(row)
-    unless User.find_by(webaccess_id: row[:username])
+    webaccess_id = row[:username].downcase
+
+    unless User.find_by(webaccess_id: webaccess_id)
       u = User.new
       u.first_name = row[:first_name]
       u.middle_name = row[:middle_name]
       u.last_name = row[:last_name]
       u.institution = 'Penn State University'
-      u.webaccess_id = row[:username]
+      u.webaccess_id = webaccess_id
       u.activity_insight_identifier = row[:user_id]
       u
     end
