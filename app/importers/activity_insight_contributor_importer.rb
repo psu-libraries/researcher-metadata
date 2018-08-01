96c7335d1d9d5f63900d07a5689fdc750d9b2ec3
Eric Durante <eric@westarete.com>
2018-08-01 16:27:25 -0400

    Update Activity Insight contributor importer
    - Make the importer work with our updated data model
    - Allow Activity Insight imports to update contributors

--- app/importers/activity_insight_contributor_importer.rb
+++ app/importers/activity_insight_contributor_importer.rb
@@ -1,35 +1,28 @@
 class ActivityInsightContributorImporter < ActivityInsightCSVImporter
   def row_to_object(row)
-    pi = PublicationImport.find_by(import_source: 'Activity Insight', source_identifier: row[:parent_id])
+    p = Publication.find_by(activity_insight_identifier: row[:parent_id])
 
-    if pi
-      unless ContributorImport.find_by(import_source: 'Activity Insight', source_identifier: row[:id])
-        ci = ContributorImport.new
-        ci.publication_import = pi
-        ci.first_name = row[:fname]
-        ci.middle_name = row[:mname]
-        ci.last_name = row[:lname]
-        ci.import_source = 'Activity Insight'
-        ci.source_identifier = row[:id]
-        ci.position = row[:ordinal].to_i
+    if p # Depends on publications being imported from Activity Insight first
+      c = Contributor.find_by(activity_insight_identifier: row[:id]) || Contributor.new
 
-        c = Contributor.new
-        c.publication = pi.publication
-        c.first_name = row[:fname]
-        c.middle_name = row[:mname]
-        c.last_name = row[:lname]
-        c.position = row[:ordinal].to_i
-        c.save!
-
-        ci
-      end
+      c.publication = p
+      c.first_name = row[:fname]
+      c.middle_name = row[:mname]
+      c.last_name = row[:lname]
+      c.position = row[:ordinal].to_i
+      c.activity_insight_identifier = row[:id] if c.new_record?
+      c
     else
       nil
     end
   end
 
   def bulk_import(objects)
-    ContributorImport.import(objects)
+    Contributor.import(objects, on_duplicate_key_update: {conflict_target: [:activity_insight_identifier],
+                                                          columns: [:first_name,
+                                                                    :middle_name,
+                                                                    :last_name,
+                                                                    :position]})
   end
 
   private
