a471eb0a99ea049e84041b25e98b9a55287f94ab
Eric Durante <eric@westarete.com>
2018-08-02 18:20:42 -0400

    Don’t persist test record
    It’s not necessary for this test, so the test will run a little faster 
    if we  don’t save the publication to the database.

--- spec/component/serializers/api/v1/publication_serializer_spec.rb
+++ spec/component/serializers/api/v1/publication_serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'component/component_spec_helper'
 
 describe API::V1::PublicationSerializer do
-  let(:publication) { create(:publication, title: 'publication 1') }
+  let(:publication) { Publication.new(title: 'publication 1') }
 
   describe "data attributes" do
     subject { serialized_data_attributes(publication) }
