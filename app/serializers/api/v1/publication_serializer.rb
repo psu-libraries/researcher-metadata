a120663c5525c6ae9eebaf5095b1102382148225
Eric Durante <eric@westarete.com>
2018-08-03 11:42:03 -0400

    Serialize more publication data for API responses
    This does not include all of the publication data that we’re currently
    storing, it just includes the subset that would be necessary to produce
    a typical citation for a publication.

--- app/serializers/api/v1/publication_serializer.rb
+++ app/serializers/api/v1/publication_serializer.rb
@@ -1,6 +1,19 @@
 module API::V1
   class PublicationSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :title
+    attributes :title, :secondary_title, :journal_title, :status, :volume, :issue,
+               :edition, :page_range, :authors_et_al
+
+    attribute :published_on do |object|
+      object.published_on.try(:iso8601)
+    end
+
+    attribute :contributors do |object|
+      object.contributors.map do |c|
+        {first_name: c.first_name,
+         middle_name: c.middle_name,
+         last_name: c.last_name}
+      end
+    end
   end
 end
--- spec/component/serializers/api/v1/publication_serializer_spec.rb
+++ spec/component/serializers/api/v1/publication_serializer_spec.rb
@@ -1,10 +1,56 @@
 require 'component/component_spec_helper'
 
 describe API::V1::PublicationSerializer do
-  let(:publication) { Publication.new(title: 'publication 1') }
+  let(:publication) { create :publication,
+                             title: 'publication 1',
+                             secondary_title: 'pub 1 subtitle',
+                             journal_title: 'prestegious journal',
+                             status: 'published',
+                             volume: '1',
+                             issue: '2',
+                             edition: '3',
+                             page_range: '4-5',
+                             authors_et_al: true,
+                             published_on: date }
+  let(:date) { nil }
 
   describe "data attributes" do
     subject { serialized_data_attributes(publication) }
     it { is_expected.to include(:title => 'publication 1') }
+    it { is_expected.to include(:secondary_title => 'pub 1 subtitle') }
+    it { is_expected.to include(:journal_title => 'prestegious journal') }
+    it { is_expected.to include(:status => 'published') }
+    it { is_expected.to include(:volume => '1') }
+    it { is_expected.to include(:issue => '2') }
+    it { is_expected.to include(:edition => '3') }
+    it { is_expected.to include(:page_range => '4-5') }
+    it { is_expected.to include(:authors_et_al => true) }
+
+    context "when the publication has a published_on date" do
+      let(:date) { Date.new(2018, 8, 3) }
+      it { is_expected.to include(:published_on => '2018-08-03') }
+    end
+
+    context "when the publication does not have a published_on date" do
+      it { is_expected.to include(:published_on => nil) }
+    end
+
+    context "when the publication does not have contributors" do
+      it { is_expected.to include(:contributors => []) }
+    end
+
+    context "when the publication has contributors" do
+      before do
+        create :contributor, first_name: 'a', middle_name: 'b', last_name: 'c', position: 2, publication: publication
+        create :contributor, first_name: 'd', middle_name: 'e', last_name: 'f', position: 1, publication: publication
+        create :contributor, first_name: 'g', middle_name: 'h', last_name: 'i', position: 3, publication: publication
+      end
+
+      subject { serialized_data_attributes(publication) }
+
+      it { is_expected.to include(:contributors => [{first_name: 'd', middle_name: 'e', last_name: 'f'},
+                                                    {first_name: 'a', middle_name: 'b', last_name: 'c'},
+                                                    {first_name: 'g', middle_name: 'h', last_name: 'i'}]) }
+    end
   end
 end
