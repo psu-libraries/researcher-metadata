5dfd74e287b7f3ca49d425c07cd4c22b2512785d
Eric Durante <eric@westarete.com>
2018-08-08 13:51:12 -0400

    Improve the integration test for the public home page

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -3,12 +3,40 @@ require 'integration/integration_spec_helper'
 feature "Home page", type: :feature do
   let(:home_content) { "PSU Libraries Research Metadata" }
 
+  shared_examples_for "a page with the public layout" do
+    it "shows a link to the home page" do
+      expect(page).to have_link 'Home', href: root_path
+    end
+
+    it "shows a link to the API documentation" do
+      expect(page).to have_link 'API'
+    end
+
+    it "shows a link to the admin interface" do
+      expect(page).to have_link 'Admin'
+    end
+  end
+
   context "when the user is logged in" do
-    before { authenticate_user }
+    before do
+      authenticate_user
+      visit root_path
+    end
 
     it "shows the home page content" do
-      visit root_path
       expect(page).to have_content home_content
     end
+
+    it_behaves_like "a page with the public layout"
+  end
+
+  context "when the user is not logged in" do
+    before { visit root_path }
+
+    it "shows the home page content" do
+      expect(page).to have_content home_content
+    end
+
+    it_behaves_like "a page with the public layout"
   end
 end
