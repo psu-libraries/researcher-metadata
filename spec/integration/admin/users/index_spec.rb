16de0f7777ea4e7388759154b7dfb0c5471d0969
Eric Durante <eric@westarete.com>
2018-08-08 14:12:09 -0400

    Create an integration test for the admin user list page

---
+++ spec/integration/admin/users/index_spec.rb
@@ -0,0 +1,44 @@
+require 'integration/integration_spec_helper'
+require 'integration/admin/shared_examples_for_admin_page'
+
+feature "Admin user list", type: :feature do
+  context "when the current user is an admin" do
+    before { authenticate_admin_user }
+
+    describe "the page content" do
+      let!(:user1) { create(:user, first_name: "Bob", last_name: "Testuser") }
+      let!(:user2) { create(:user, first_name: "Susan", last_name: "Tester") }
+
+      before { visit 'admin/user' }
+
+      it "shows the user list heading" do
+        expect(page).to have_content 'List of Users'
+      end
+
+      it "shows information about each user" do
+        expect(page).to have_content user1.id
+        expect(page).to have_content 'Bob'
+        expect(page).to have_content 'Testuser'
+
+        expect(page).to have_content user2.id
+        expect(page).to have_content 'Susan'
+        expect(page).to have_content 'Tester'
+      end
+    end
+
+    describe "the page layout" do
+      before { visit 'admin/user' }
+
+      it_behaves_like "a page with the admin layout"
+    end
+  end
+
+  context "when the current user is not an admin" do
+    before { authenticate_user }
+    it "redirects back to the home page with an error message" do
+      visit 'admin/user'
+      expect(page.current_path).to eq root_path
+      expect(page).to have_content I18n.t('admin.authorization.not_authorized')
+    end
+  end
+end
