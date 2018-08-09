9f96f82fcef0618b0a43ab687fac9127d5381e2e
Eric Durante <eric@westarete.com>
2018-08-09 12:05:23 -0400

    Remove unneeded columns from duplicate_publication_groups table
    I thought that I was going to use these columns for something, but I 
    didn’t after all.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_215350) do
+ActiveRecord::Schema.define(version: 2018_08_09_160107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,10 +37,6 @@ ActiveRecord::Schema.define(version: 2018_08_08_215350) do
   end
 
   create_table "duplicate_publication_groups", force: :cascade do |t|
-    t.text "title"
-    t.text "journal"
-    t.string "issue"
-    t.string "volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
