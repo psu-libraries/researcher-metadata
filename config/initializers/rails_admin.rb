0c6066211a506b2c74dd892ce61fc45f37a8f1d7
Eric Durante <eric@westarete.com>
2018-08-29 15:17:26 -0400

    Remove redundant conditional

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -11,7 +11,7 @@ RailsAdmin.config do |config|
   config.authorize_with do
     unless current_user.admin?
       flash[:alert] = I18n.t('admin.authorization.not_authorized')
-      redirect_to main_app.root_path unless current_user.is_admin
+      redirect_to main_app.root_path
     end
   end
 
