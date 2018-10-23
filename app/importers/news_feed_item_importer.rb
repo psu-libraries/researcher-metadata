f613edc3a670bf0b43116e04eee5676d701b4f4c
Alexander Kiessling <ajkiessl@gmail.com>
2018-10-23 17:08:46 -0400

    Changes to news importer.  Find_or_create_by and variable renaming.

--- app/importers/news_feed_item_importer.rb
+++ app/importers/news_feed_item_importer.rb
@@ -6,11 +6,9 @@ class NewsFeedItemImporter
   # parse rss feeds from news.psu.edu
   def call
     selector = "//a[starts-with(@href, \"mailto:\")]/@href"
-    rss_feeds = get_rss_feeds
     rss_feeds.each do |feed|
       rss = RSS::Parser.parse(open(feed).read, false).items
       rss.each do |result|
-        puts result
         html_doc = Nokogiri::HTML(open(result.link))
         nodes = html_doc.xpath selector
         addresses = nodes.collect {|n| n.value[7..-1]}
@@ -18,20 +16,16 @@ class NewsFeedItemImporter
           if a.end_with?("psu.edu")
             if local_user(a) ==  true
               webaccess_id = a[0..-9]
-              #puts webaccess_id
               u = User.find_by(webaccess_id: webaccess_id)
 
-              nfi = NewsFeedItem.new(user:        u,
-                                     title:       result.title,
-                                     url:         result.link,
-                                     pubdate:     result.pubDate,
-                                     description: result.description)
-
-              begin
-                nfi.validate!
-                nfi.save!
-              rescue ActiveRecord::RecordNotUnique => e
+              nfi = NewsFeedItem.find_or_create_by(url: result.link) do |item|
+                item.user = u
+                item.title = result.title
+                item.url = result.link
+                item.pubdate = result.pubDate
+                item.description = result.description
               end
+
             end
           end
         end
@@ -49,7 +43,7 @@ class NewsFeedItemImporter
     end
   end
 
-  def get_rss_feeds
+  def rss_feeds
     return ['https://news.psu.edu/rss/topic/research', 'https://news.psu.edu/rss/topic/academic', 'https://news.psu.edu/rss/topic/impact', 'https://news.psu.edu/rss/topic/campus-life']
   end
 end
