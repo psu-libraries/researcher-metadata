c3bda3d96e87dad8c94a7d2742f8982f73398423
Alexander Kiessling <ajkiessl@gmail.com>
2018-11-19 12:22:39 -0500

    Fixed contract importer to account for duplicate status rows.

--- app/importers/activity_insight_contract_importer.rb
+++ app/importers/activity_insight_contract_importer.rb
@@ -1,6 +1,6 @@
 class ActivityInsightContractImporter < ActivityInsightCSVImporter
   def row_to_object(row)
-    if row[:ospkey].present? && row[:status] == 'Awarded'
+    if row[:ospkey].present? && status(row) == 'Awarded'
       u = User.find_by(webaccess_id: row[:username].downcase)
 
       ci = ContractImport.find_by(activity_insight_id: row[:id]) ||
@@ -33,12 +33,29 @@ class ActivityInsightContractImporter < ActivityInsightCSVImporter
       title: row[:title],
       contract_type: row[:type],
       sponsor: row[:sponorg],
-      status: row[:status],
+      status: status(row),
       amount: row[:amount],
       ospkey: row[:ospkey],
       award_start_on: row[:award_start],
       award_end_on: row[:award_end],
     }
+  end
+
+  def status(row)
+    extract_value(row: row, header_key: :status, header_count: 2)
+  end
+
+  def extract_value(row:, header_key:, header_count:)
+    value = nil
+    header_count.times do |i|
+      if i == 0
+        value = row[header_key] if row[header_key].present? && row[header_key].to_s.downcase != 'other'
+      else
+        key = header_key.to_s + (i+1).to_s
+        value = row[key.to_sym] if row[key.to_sym].present? && row[key.to_sym].to_s.downcase != 'other'
+      end
+    end
+    value
   end
 
 end
