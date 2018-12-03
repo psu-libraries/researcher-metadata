c261f413d737b08aa7e572e3767235f4a5a621db
Alexander Kiessling <ajkiessl@gmail.com>
2018-12-03 11:14:43 -0500

    Screening importer and importer spec added.  File to be imported added.  Building rake task to run importer.

---
+++ spec/component/importers/activity_insight_performance_screening_importer_spec.rb
@@ -0,0 +1,50 @@
+require 'component/component_spec_helper'
+
+describe ActivityInsightPerformanceScreeningImporter do
+  let(:importer) { ActivityInsightPerformanceScreeningImporter.new(filename: filename) }
+
+  before(:each) do
+    Performance.create( title: "Title 1", activity_insight_id: 161819957248 )
+    Performance.create( title: "Title 2", activity_insight_id: 161395374080 )
+    Performance.create( title: "Title 3", activity_insight_id: 160440864768 )
+    Performance.create( title: "Title 4", activity_insight_id: 151334516736 )
+  end
+
+
+  describe '#call' do
+    context "when given a well-formed .csv file of valid performance screening data from Activity Insight" do
+      let(:filename) { Rails.root.join('spec', 'fixtures', 'ai_performance_screenings.csv') }
+
+      it "creates a new performance_screening record for every valid row in the .csv file" do
+        expect { importer.call }.to change { PerformanceScreening.count }.by 5
+
+        p1 = PerformanceScreening.first
+        p2 = PerformanceScreening.second
+        p3 = PerformanceScreening.third
+        p4 = PerformanceScreening.fourth
+        p5 = PerformanceScreening.fifth
+
+        expect(p1.screening_type).to eq 'Open Exhibit'
+        expect(p1.name).to eq 'Center For Media Innovation / Point Park University'
+        expect(p1.location).to eq nil
+
+        expect(p2.screening_type).to eq 'Open Exhibit'
+        expect(p2.name).to eq 'Palmer Museum of Art'
+        expect(p2.location).to eq nil
+
+        expect(p3.screening_type).to eq nil
+        expect(p3.name).to eq nil
+        expect(p3.location).to eq nil
+
+        expect(p4.screening_type).to eq 'Invited'
+        expect(p4.name).to eq 'International Communcation Association'
+        expect(p4.location).to eq 'Prague, Czech Republic'
+
+        expect(p5.screening_type).to eq 'Juried'
+        expect(p5.name).to eq 'Collegetown Film Festival'
+        expect(p5.location).to eq 'Athen, OH'
+      end
+    end
+  end
+end
+
