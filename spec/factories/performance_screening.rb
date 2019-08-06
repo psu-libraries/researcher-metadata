83c39e73adc8c1d78ba47fc95e2abf1429b30694
Eric Durante <eric@westarete.com>
2019-08-06 15:38:58 -0400

    Use dynamic attributes in Factory Bot factories
    The static attributes that we were using here were deprecated and
    they’re no longer supported in the next version of Factory Bot.

--- spec/factories/performance_screening.rb
+++ spec/factories/performance_screening.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :performance_screening do
     performance { create :performance }
-    screening_type 'Invited'
-    name 'Test Performance'
-    location 'City, State'
+    screening_type { 'Invited' }
+    name { 'Test Performance' }
+    location { 'City, State' }
     sequence( :activity_insight_id ) { |n| 1000000000 + n }
   end
 end
