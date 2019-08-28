885aedc1af1541902363a168900263b446234959
Eric Durante <eric@westarete.com>
2019-08-28 10:16:28 -0400

    Revert "Always import publication DOIs from Pure"
    This reverts commit c2ecb010373843950360d5ab3c346ec51a1aa350.

--- app/importers/pure_publication_importer.rb
+++ app/importers/pure_publication_importer.rb
@@ -33,8 +33,7 @@ class PurePublicationImporter
 
               if pi.persisted?
                 if p.updated_by_user_at.present?
-                  pi.publication.update_attributes!(total_scopus_citations: publication['totalScopusCitations'],
-                                                    doi: doi(publication))
+                  pi.publication.update_attributes!(total_scopus_citations: publication['totalScopusCitations'])
                 else
                   pi.publication.update_attributes!(pub_attrs(publication))
                 end
