009fb73ad7a8242cc8b0a194d4c78113a359519d
Eric Durante <eric@westarete.com>
2019-09-04 16:06:07 -0400

    Test that all of the grant attributes are being imported from WoS

--- spec/component/importers/web_of_science_file_importer_spec.rb
+++ spec/component/importers/web_of_science_file_importer_spec.rb
@@ -33,9 +33,14 @@ describe WebOfScienceFileImporter do
           end
           it "creates new grants where agency name and ID information are available" do
             expect { importer.call }.to change { Grant.count }.by 1
+            expect(Grant.find_by(agency_name: 'National Science Foundation',
+                                 identifier: 'ATMO-0803779')).not_to be_nil
           end
           it "creates new research fund records to associate the new grants with the publications" do
             expect { importer.call }.to change { ResearchFund.count }.by 1
+            new_grant = Grant.find_by(agency_name: 'National Science Foundation',
+                                      identifier: 'ATMO-0803779')
+            expect(pub.grants).to eq [new_grant]
           end
         end
         context "when existing grants match the data for matching publications" do
