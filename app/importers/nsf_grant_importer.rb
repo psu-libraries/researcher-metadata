9bc06e6818851697a0fc73087a2a06fcfb93f058
Eric Durante <eric@westarete.com>
2019-09-25 17:12:22 -0400

    Import NSF data one year at a time
    Each NSF grant is represented in its own separate XML file, which means
    that there are a huge number of files in total. It’s much more
    manageable to have the files grouped by year.

--- app/importers/nsf_grant_importer.rb
+++ app/importers/nsf_grant_importer.rb
@@ -6,35 +6,39 @@ class NSFGrantImporter
   end
 
   def call
-    import_files = Dir.children(dirname).select { |f| File.extname(f) == '.xml' }
-    pbar = ProgressBar.create(title: 'Importing NSF Grant Data', total: import_files.count) unless Rails.env.test?
-    import_files.each do |file|
-      nsf_grant = NSFGrant.new(File.open(dirname.join(file)) { |f| Nokogiri::XML(f) })
+    import_dirs = Dir.children(dirname).select { |f| File.directory?(dirname.join(f)) }.sort
 
-      if nsf_grant.importable?
-        g = Grant.find_by(agency_name: nsf_grant.agency_name, identifier: nsf_grant.identifier) || Grant.new
-        g.identifier = nsf_grant.identifier
-        g.title = nsf_grant.title
-        g.start_date = nsf_grant.start_date
-        g.end_date = nsf_grant.end_date
-        g.abstract = nsf_grant.abstract
-        g.amount_in_dollars = nsf_grant.amount_in_dollars
-        g.agency_name = nsf_grant.agency_name
-        g.save!
+    import_dirs.each do |d|
+      import_files = Dir.children(dirname.join(d)).select { |f| File.extname(f) == '.xml' }
+      pbar = ProgressBar.create(title: "Importing NSF Grant Data for #{d}", total: import_files.count) unless Rails.env.test?
+      import_files.each do |file|
+        nsf_grant = NSFGrant.new(File.open(dirname.join(d).join(file)) { |f| Nokogiri::XML(f) })
 
-        users = User.find_by_nsf_grant(nsf_grant)
-        users.each do |u|
-          unless ResearcherFund.find_by(user: u, grant: g)
-            rf = ResearcherFund.new
-            rf.user = u
-            rf.grant = g
-            rf.save!
+        if nsf_grant.importable?
+          g = Grant.find_by(agency_name: nsf_grant.agency_name, identifier: nsf_grant.identifier) || Grant.new
+          g.identifier = nsf_grant.identifier
+          g.title = nsf_grant.title
+          g.start_date = nsf_grant.start_date
+          g.end_date = nsf_grant.end_date
+          g.abstract = nsf_grant.abstract
+          g.amount_in_dollars = nsf_grant.amount_in_dollars
+          g.agency_name = nsf_grant.agency_name
+          g.save!
+
+          users = User.find_by_nsf_grant(nsf_grant)
+          users.each do |u|
+            unless ResearcherFund.find_by(user: u, grant: g)
+              rf = ResearcherFund.new
+              rf.user = u
+              rf.grant = g
+              rf.save!
+            end
           end
         end
+        pbar.increment unless Rails.env.test?
       end
-      pbar.increment unless Rails.env.test?
+      pbar.finish unless Rails.env.test?
     end
-    pbar.finish unless Rails.env.test?
   end
 
   private
