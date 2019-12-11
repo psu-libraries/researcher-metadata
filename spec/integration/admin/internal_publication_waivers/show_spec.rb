455bc1f0942dcaf3a6507e9ba9ba0bfa7448cb5b
Eric Durante <eric@westarete.com>
2019-12-11 16:25:32 -0500

    Add integration tests for new admin pages for waivers

---
+++ spec/integration/admin/internal_publication_waivers/show_spec.rb
@@ -0,0 +1,48 @@
+require 'integration/integration_spec_helper'
+require 'integration/admin/shared_examples_for_admin_page'
+
+feature "Admin internal publication waiver detail page", type: :feature do
+  let!(:waiver) { create :internal_publication_waiver, authorship: auth, reason_for_waiver: "Just because." }
+  let!(:auth) { create :authorship, publication: pub, user: user }
+  let!(:user) { create :user, first_name: "Joe", last_name: "Testerson" }
+  let!(:pub) { create :publication, title: "Publication One" }
+
+  context "when the current user is an admin" do
+    before { authenticate_admin_user }
+
+    describe "the page content" do
+      before { visit rails_admin.show_path(model_name: :internal_publication_waiver, id: waiver.id) }
+
+      it "shows the waiver detail heading" do
+        expect(page).to have_content "Details for Internal publication waiver 'Publication One'"
+      end
+
+      it "shows the title of the publication associated with the waiver" do
+        expect(page).to have_content "Publication One"
+      end
+
+      it "shows the name of the user associated with the waiver" do
+        expect(page).to have_content "Joe Testerson"
+      end
+
+      it "shows the reason for the waiver" do
+        expect(page).to have_content "Just because."
+      end
+    end
+
+    describe "the page layout" do
+      before { visit rails_admin.show_path(model_name: :internal_publication_waiver, id: waiver.id) }
+
+      it_behaves_like "a page with the admin layout"
+    end
+  end
+
+  context "when the current user is not an admin" do
+    before { authenticate_user }
+    it "redirects back to the home page with an error message" do
+      visit rails_admin.show_path(model_name: :internal_publication_waiver, id: waiver.id)
+      expect(page.current_path).to eq root_path
+      expect(page).to have_content I18n.t('admin.authorization.not_authorized')
+    end
+  end
+end
