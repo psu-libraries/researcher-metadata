4843fd882dc57e42122cab0847f3040dd6e79796
Alex Kiessling <ajkiessl@gmail.com>
2020-01-13 13:21:08 -0500

    Added migration to enable pg_trgm.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_170847) do
+ActiveRecord::Schema.define(version: 2020_01_13_181629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
