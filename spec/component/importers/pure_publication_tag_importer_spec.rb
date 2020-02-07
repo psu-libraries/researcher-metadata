1a95e4054be891c13c55c505c86cc8f7fcd732df
Eric Durante <eric@westarete.com>
2020-02-07 14:49:15 -0500

    Try the pure fingerprint importer with matching data in the database
    This is running against fixture data that is a sample of real data from
    v5.15 of the Pure API. It now reveals that the importer (originally
    written to import data from v5.11 of the Pure API is incompatible with
    the new data.

--- spec/component/importers/pure_publication_tag_importer_spec.rb
+++ spec/component/importers/pure_publication_tag_importer_spec.rb
@@ -7,8 +7,28 @@ describe PurePublicationTagImporter do
     context "when given a well-formed .json file of publicaiton fingerprint data from Pure" do
       let(:filename) { Rails.root.join('spec', 'fixtures', 'pure_publication_fingerprints.json') }
 
-      it "runs" do
-        expect { importer.call }.not_to raise_error
+      context "when no publications in the database match the publication data being imported" do
+        it "runs" do
+          expect { importer.call }.not_to raise_error
+        end
+      end
+
+      context "when publications in the database match the publication data being imported" do
+        let!(:imp1) { create :publication_import,
+                             source: "Pure",
+                             source_identifier: "e1b21d75-4579-4efc-9fcc-dcd9827ee51a",
+                             publication: pub1 }
+        let!(:imp2) { create :publication_import,
+                             source: "Pure",
+                             source_identifier: "890420eb-eff9-4cbc-8e1b-20f68460f4eb",
+                             publication: pub2 }
+
+        let!(:pub1) { create :publication }
+        let!(:pub2) { create :publication }
+
+        it "runs" do
+          expect { importer.call }.not_to raise_error
+        end
       end
     end
   end
