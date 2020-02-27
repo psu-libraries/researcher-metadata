fc61b0b65af5b70631d0b72553ed4e6dfab131b4
Eric Durante <eric@westarete.com>
2020-02-27 10:45:40 -0500

    Extract method

--- app/models/orcid_oauth_client.rb
+++ app/models/orcid_oauth_client.rb
@@ -6,13 +6,19 @@ class OrcidOauthClient
     request = {
       headers: {"Accept" => "application/json"},
       body: {
-        client_id: Rails.configuration.x.orcid['client_id'],
-        client_secret: Rails.configuration.x.orcid['client_secret'],
+        client_id: orcid_config['client_id'],
+        client_secret: orcid_config['client_secret'],
         grant_type: 'authorization_code',
         code: code
       }
     }
 
     self.class.post("/token", request)
+  end
+
+  private
+
+  def orcid_config
+    Rails.configuration.x.orcid
   end
 end
