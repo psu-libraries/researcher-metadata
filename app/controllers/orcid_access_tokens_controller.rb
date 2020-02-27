dc3509201a9cb9314457518cd90ef473c557d6e0
Eric Durante <eric@westarete.com>
2020-02-27 11:08:57 -0500

    Handle OAuth edge-case with a message to the user

--- app/controllers/orcid_access_tokens_controller.rb
+++ app/controllers/orcid_access_tokens_controller.rb
@@ -2,7 +2,10 @@ class OrcidAccessTokensController < UserController
   before_action :authenticate!
 
   def new
-    unless current_user.orcid_access_token.present?
+    if current_user.orcid_access_token.present?
+      flash[:notice] = "Your ORCiD account is already linked to your metadata profile."
+      redirect_to profile_bio_path
+    else
       redirect_to "https://sandbox.orcid.org/oauth/authorize?client_id=#{Rails.configuration.x.orcid['client_id']}&response_type=code&scope=/activities/update&redirect_uri=#{URI::encode(orcid_access_token_url)}"
     end
   end
