49502eb2c35cb9f22784d221776b94f6a514a5ae
Eric Durante <eric@westarete.com>
2020-04-29 14:14:05 -0400

    Update some naming to be consistent with the new project name

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 ENV['RAILS_ADMIN_THEME'] = 'material'
 
-module PsuResearchMetadata
+module ResearcherMetadata
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
