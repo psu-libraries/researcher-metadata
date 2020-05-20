c0eb42643e32a678d7efff4d2293d3842b5a1005
Eric Durante <eric@westarete.com>
2020-05-20 14:58:38 -0400

    Use proper wording to describe ORCID concepts

--- app/controllers/orcid_access_tokens_controller.rb
+++ app/controllers/orcid_access_tokens_controller.rb
@@ -3,7 +3,7 @@ class OrcidAccessTokensController < UserController
 
   def new
     if current_user.orcid_access_token.present?
-      flash[:notice] = "Your ORCiD account is already linked to your metadata profile."
+      flash[:notice] = "Your ORCID record is already linked to your metadata profile."
       redirect_to profile_bio_path
     else
       redirect_to "https://sandbox.orcid.org/oauth/authorize?client_id=#{Rails.configuration.x.orcid['client_id']}&response_type=code&scope=/activities/update&redirect_uri=#{URI::encode(orcid_access_token_url)}"
@@ -16,9 +16,9 @@ class OrcidAccessTokensController < UserController
     if response.code == 200
       response_body = JSON.parse(response.to_s)
       current_user.update_attributes!(orcid_access_token: response['access_token'])
-      flash[:notice] = "Your ORCiD account was successfully linked to your metadata profile."
+      flash[:notice] = "Your ORCID record was successfully linked to your metadata profile."
     else
-      flash[:alert] = "There was an error linking your ORCiD account to your metadata profile."
+      flash[:alert] = "There was an error linking your ORCID record to your metadata profile."
     end
 
     redirect_to profile_bio_path
