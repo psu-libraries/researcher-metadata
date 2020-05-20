45db00cf1935fcce704d6e40ef833aea1ddd462d
Eric Durante <eric@westarete.com>
2020-05-20 17:48:19 -0400

    Move ORCID Oauth flash messages into translations

--- app/controllers/orcid_access_tokens_controller.rb
+++ app/controllers/orcid_access_tokens_controller.rb
@@ -3,7 +3,7 @@ class OrcidAccessTokensController < UserController
 
   def new
     if current_user.orcid_access_token.present?
-      flash[:notice] = "Your ORCID record is already linked to your metadata profile."
+      flash[:notice] = I18n.t('profile.orcid_access_tokens.new.already_linked')
       redirect_to profile_bio_path
     else
       redirect_to "https://sandbox.orcid.org/oauth/authorize?client_id=#{Rails.configuration.x.orcid['client_id']}&response_type=code&scope=/activities/update&redirect_uri=#{URI::encode(orcid_access_token_url)}"
@@ -16,9 +16,9 @@ class OrcidAccessTokensController < UserController
     if response.code == 200
       response_body = JSON.parse(response.to_s)
       current_user.update_attributes!(orcid_access_token: response['access_token'])
-      flash[:notice] = "Your ORCID record was successfully linked to your metadata profile."
+      flash[:notice] = I18n.t('profile.orcid_access_tokens.create.success')
     else
-      flash[:alert] = "There was an error linking your ORCID record to your metadata profile."
+      flash[:alert] = I18n.t('profile.orcid_access_tokens.create.error')
     end
 
     redirect_to profile_bio_path
