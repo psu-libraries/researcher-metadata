9f0c15ae0f51ab1eb0a3128c25e56efcbd727c7a
Eric Durante <eric@westarete.com>
2020-05-21 11:15:57 -0400

    Finish testing ORCID access token controller

--- spec/component/controllers/orcid_access_tokens_controller_spec.rb
+++ spec/component/controllers/orcid_access_tokens_controller_spec.rb
@@ -22,6 +22,16 @@ describe OrcidAccessTokensController, type: :controller do
           expect(response).to redirect_to profile_bio_path
         end
       end
+
+      context "when the user does not have an ORCID access token" do
+        let(:token) { nil }
+        
+        it "redirects to the start of the ORCID Oauth page" do
+          get :new
+          
+          expect(response).to redirect_to "https://sandbox.orcid.org/oauth/authorize?client_id=test&response_type=code&scope=/activities/update&redirect_uri=http://test.host/orcid_access_token"
+        end
+      end
     end
 
     context "when the user is not authenticated" do
@@ -35,10 +45,50 @@ describe OrcidAccessTokensController, type: :controller do
 
   describe '#create' do
     context "when the user is authenticated" do
+      let(:client) { double 'ORCID Oauth client' }
+      let(:response) { double 'ORCID Oauth response',
+                              code: code,
+                              content_type: nil }
+
+      before do
+        allow(OrcidOauthClient).to receive(:new).and_return(client)
+        allow(client).to receive(:create_token).with('abc123').and_return(response)
+        allow(response).to receive(:[]).with('access_token').and_return('xyz789')
+      end
+
       let!(:user) { create :user }
-      before { authenticate_as(user) }
+      before do
+        authenticate_as(user)
+        post :create, params: {code: 'abc123'}
+      end
       
-      xit
+      context "when the request to create an access token is successful" do
+        let(:code) { 200 }
+
+        it "saves the access token" do
+          expect(user.orcid_access_token).to eq 'xyz789'
+        end
+
+        it "sets a flash message" do
+          expect(flash[:notice]).to eq I18n.t('profile.orcid_access_tokens.create.success')
+        end
+
+        it "redirects back to the profile bio page" do
+          expect(response).to redirect_to profile_bio_path
+        end
+      end
+
+      context "when the request to create an access token fails" do
+        let(:code) { 400 }
+
+        it "sets a flash message" do
+          expect(flash[:alert]).to eq I18n.t('profile.orcid_access_tokens.create.error')
+        end
+
+        it "redirects back to the profile bio page" do
+          expect(response).to redirect_to profile_bio_path
+        end
+      end
     end
 
     context "when the user is not authenticated" do
