3c9a5e1ab705ec637140efea6a9cf95d050f92f6
Eric Durante <eric@westarete.com>
2020-05-21 13:07:52 -0400

    Move more flash message content to translations

--- app/controllers/orcid_employments_controller.rb
+++ app/controllers/orcid_employments_controller.rb
@@ -5,20 +5,20 @@ class OrcidEmploymentsController < UserController
     membership = current_user.primary_organization_membership
 
     if membership.orcid_resource_identifier.present?
-      flash[:notice] = "The employment record has already been added to your ORCID profile."
+      flash[:notice] = I18n.t('profile.orcid_employments.create.already_added')
     else
       employment = OrcidEmployment.new(membership)
       employment.save!
       membership.update_attributes!(orcid_resource_identifier: employment.location)
 
-      flash[:notice] = "The employment record was successfully added to your ORCiD profile."
+      flash[:notice] = I18n.t('profile.orcid_employments.create.success')
     end
     
   rescue OrcidEmployment::InvalidToken
         current_user.clear_orcid_access_token
-        flash[:alert] = "Your ORCiD account is no longer linked to your metadata profile."
+        flash[:alert] = I18n.t('profile.orcid_employments.create.account_not_linked')
   rescue OrcidEmployment::FailedRequest
-        flash[:alert] = "There was an error adding your employment history to your ORCiD profile."
+        flash[:alert] = I18n.t('profile.orcid_employments.create.error')
   ensure
     redirect_to profile_bio_path
   end
