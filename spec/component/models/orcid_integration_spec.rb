1817247bba4f289a565bfe0d1de5596fe33b9956
Alex Kiessling <ajkiessl@gmail.com>
2020-06-10 16:18:12 -0400

    fixed comment

--- spec/component/models/orcid_integration_spec.rb
+++ spec/component/models/orcid_integration_spec.rb
@@ -3,7 +3,7 @@ require 'component/component_spec_helper'
 describe OrcidAPIClient do
   let(:client) { OrcidAPIClient.new(resource) }
   # Access token and orcid id for orcidtest@pennteam.m8r.co sandbox account
-  # Using ORCID's disambiguated-identifier number for testing
+  # Using ORCID's disambiguated-organization-identifier number for testing
   let(:resource) { double 'ORCID Resource',
                           access_token: "98768b7f-c177-4c24-9fe8-b575997e2bc7",
                           orcid_id: "0000-0002-5925-6081",
