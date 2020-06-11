e68a075c4f9f5b4ddb440fa122a6271a8ba60dba
Alex Kiessling <ajkiessl@gmail.com>
2020-06-11 14:08:03 -0400

    gets record after posting and compares.  Deletes record after test

--- spec/component/models/orcid_integration_spec.rb
+++ spec/component/models/orcid_integration_spec.rb
@@ -28,8 +28,30 @@ describe OrcidAPIClient do
   }
 
   describe "integrating with ORCID", glacial: true do
+    let(:headers) {
+      {
+        headers:
+          { "Content-type" => "application/vnd.orcid+json",
+            "Authorization" => "Bearer #{resource.access_token}"
+          }
+      }
+    }
+    let(:employments_uri) { "#{OrcidAPIClient.base_uri}/#{resource.orcid_id}/employments" }
+    let(:get_employments) { client.class.get(employments_uri, headers).parsed_response }
+    let(:employments_hash) { Hash.from_xml(get_employments) }
+    let(:json_resource) { JSON.parse(resource.to_json.gsub('-', '_')) }
+    let(:employment_summary) { employments_hash["employments"]["affiliation_group"].last["employment_summary"] }
+
+    after do
+      delete_employ_uri = employments_uri[0..-2].to_s + "/#{employment_summary["put_code"]}"
+      client.class.delete(delete_employ_uri, headers)
+    end
+
     it "successfully POSTs to ORCID and does not return an error" do
       expect(client.post.parsed_response).to eq nil
+      expect(employment_summary["department_name"]).to eq json_resource["department_name"]
+      expect(employment_summary["role_title"]).to eq json_resource["role_title"]
+      expect(employment_summary["organization"]).to eq json_resource["organization"]
     end
   end
 end
