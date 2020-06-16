984bfca475b5c2aec455add8d6a818aa6cba3ce8
Eric Durante <ericdurante@gettis.local>
2020-06-16 14:36:35 -0400

    Add column to authorships table to track open access notifications

---
+++ db/migrate/20200616183430_add_open_access_notification_timestamp_to_authorships.rb
@@ -0,0 +1,5 @@
+class AddOpenAccessNotificationTimestampToAuthorships < ActiveRecord::Migration[5.2]
+  def change
+    add_column :authorships, :open_access_notification_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_150305) do
+ActiveRecord::Schema.define(version: 2020_06_16_183430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_150305) do
     t.boolean "confirmed", default: true
     t.datetime "scholarsphere_uploaded_at"
     t.string "role"
+    t.datetime "open_access_notification_sent_at"
     t.index ["publication_id"], name: "index_authorships_on_publication_id"
     t.index ["user_id"], name: "index_authorships_on_user_id"
   end
