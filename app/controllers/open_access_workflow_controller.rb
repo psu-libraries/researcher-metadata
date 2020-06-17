11dc66876fdeaeb7e36c18ff2a3ffb44271b519e
Eric Durante <ericdurante@gettis.home>
2020-06-17 10:42:34 -0400

    Refactor to simplify controller logic
    This is actually a prelude to partially changing the behavior of these
    controllers.

--- app/controllers/open_access_workflow_controller.rb
+++ app/controllers/open_access_workflow_controller.rb
@@ -7,13 +7,11 @@ class OpenAccessWorkflowController < UserController
   private
 
   def publication
-    @publication ||= current_user.publications
-      .where(%{(open_access_url IS NULL OR open_access_url = '') AND (user_submitted_open_access_url IS NULL OR user_submitted_open_access_url = '')})
-      .find(params[:id])
+    @publication ||= current_user.publications.find(params[:id])
   end
 
   def raise_if_inaccessible
-    if publication.scholarsphere_upload_pending? || publication.open_access_waived?
+    if publication.has_open_access_information?
       raise ActiveRecord::RecordNotFound
     end
   end
