8005023ab9fe48d4d901bb7063a32a2d26336e08
Eric Durante <ericdurante@gettis.home>
2020-08-07 16:06:42 -0400

    Add database indices to date field on publications
    These will greatly speed up the detection of possible duplicate
    publications.

---
+++ db/migrate/20200807195118_add_indices_to_publications.rb
@@ -0,0 +1,11 @@
+class AddIndicesToPublications < ActiveRecord::Migration[5.2]
+  def up
+    add_index :publications, :published_on
+    execute(%{CREATE INDEX index_publications_on_published_on_year ON publications USING btree (EXTRACT(YEAR FROM published_on));})
+  end
+
+  def down
+    remove_index :publications, :published_on
+    remove_index :publications, name: :index_publications_on_published_on_year
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_191346) do
+ActiveRecord::Schema.define(version: 2020_08_07_195118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -343,9 +343,11 @@ ActiveRecord::Schema.define(version: 2020_06_26_191346) do
     t.text "open_access_url"
     t.datetime "open_access_button_last_checked_at"
     t.text "user_submitted_open_access_url"
+    t.index "date_part('year'::text, published_on)", name: "index_publications_on_published_on_year"
     t.index ["doi"], name: "index_publications_on_doi"
     t.index ["duplicate_publication_group_id"], name: "index_publications_on_duplicate_publication_group_id"
     t.index ["issue"], name: "index_publications_on_issue"
+    t.index ["published_on"], name: "index_publications_on_published_on"
     t.index ["volume"], name: "index_publications_on_volume"
   end
 
