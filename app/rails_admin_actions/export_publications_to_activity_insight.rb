b7b5a865ceaf93fd1769e26ecf5e095040378ef4
Alex Kiessling <ajkiessl@gmail.com>
2020-09-11 15:33:23 -0400

    created export_publications_to_activity_insight action.  Added get http method response in controller.  Button passing params in index by org view.  This action can only be accessed from publications by org index.  Some info on integration page, buttons don't do anything yet but they are there.

---
+++ app/rails_admin_actions/export_publications_to_activity_insight.rb
@@ -0,0 +1,46 @@
+module RailsAdmin
+  module Config
+    module Actions
+      class ExportPublicationsToActivityInsight < RailsAdmin::Config::Actions::Base
+        register_instance_option :collection? do
+          true
+        end
+
+        register_instance_option :pjax? do
+          false
+        end
+
+        register_instance_option :visible? do
+          false
+        end
+
+        register_instance_option :http_methods do
+          [:get, :post]
+        end
+
+        register_instance_option :controller do
+          proc do
+            associations = model_config.list.fields.select { |f| f.try(:eager_load?) }.collect { |f| f.association.name }
+            options = {}
+            options = options.merge(include: associations) unless associations.blank?
+            options = options.merge(get_sort_hash(model_config))
+            options = options.merge(query: params[:query]) if params[:query].present?
+            options = options.merge(filters: params[:f]) if params[:f].present?
+            options = options.merge(bulk_ids: params[:bulk_ids]) if params[:bulk_ids]
+            scope = Organization.find(params[:org_id]).all_publications.includes(:organizations)
+            if auth_scope = @authorization_adapter && @authorization_adapter.query(:index, model_config.abstract_model)
+              scope = scope.merge(auth_scope)
+            end
+
+            @objects ||= model_config.abstract_model.all(options, scope)
+            if request.get?
+              render :export_publications_to_activity_insight
+            elsif request.post?
+              redirect_to '/'
+            end
+          end
+        end
+      end
+    end
+  end
+end
