636809a9cf81596bc5aa20856a78dd6068a3c011
Eric Durante <ericdurante@gettis.home>
2020-12-04 17:04:24 -0500

    WIP on importing publisher data from Pure
    There’s no test for this importer yet.
    
    This is also laying the groundwork for automating the import of other
    data from Pure.

---
+++ app/importers/pure_importer.rb
@@ -0,0 +1,21 @@
+class PureImporter
+  private
+
+  def base_url
+    "https://pennstate.pure.elsevier.com/ws/api/516/"
+  end
+
+  def pure_api_key
+    Rails.configuration.x.pure['api_key']
+  end
+
+  def total_records
+    get_records(type: record_type, page_size: 1, offset: 0)['count']
+  end
+
+  def get_records(type:, page_size:, offset:)
+    JSON.parse(HTTParty.get("#{base_url}/#{type}?navigationLink=false&size=#{page_size}&offset=#{offset}",
+                            headers: {"api-key" => pure_api_key,
+                                      "Accept" => "application/json"}).to_s)
+  end
+end
