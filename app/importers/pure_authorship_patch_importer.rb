97e19acf69a2c0371c170b57b33d55821755895e
Eric Durante <ericdurante@gettis.local>
2021-01-26 14:33:42 -0500

    Create temporary authorship import tasks
    We discovered that some data in the production database about
    publication authorships had been lost in the process of merging
    duplicate publication records. These new importers should re-import any
    such authorship information that might have been lost. We’ve also made
    changes to the publication merging process so that authorship data
    should not be lost in the same way during future merges. With that being
    the case, we just need to run these importers one time in production,
    and then they can be totally removed.

---
+++ app/importers/pure_authorship_patch_importer.rb
@@ -0,0 +1,45 @@
+class PureAuthorshipPatchImporter < PurePublicationImporter
+  def call
+    import_files = Dir.children(dirname)
+    pbar = ProgressBar.create(title: 'Importing Pure Publications', total: import_files.count) unless Rails.env.test?
+    import_files.each do |filename|
+      File.open(dirname.join(filename), 'r') do |file|
+        MultiJson.load(file)['items'].each do |publication|
+          if publication['type']['term']['text'].detect { |t| t['locale'] == 'en_US' }['value'] == "Article"
+
+            ActiveRecord::Base.transaction do
+              pi = PublicationImport.find_by(source: PurePublicationImporter::IMPORT_SOURCE,
+                                             source_identifier: publication['uuid'])
+
+              p = pi.publication
+
+              authorships = publication['personAssociations'].select do |a|
+                !a['authorCollaboration'].present? &&
+                  a['personRole']['term']['text'].detect { |t| t['locale'] == 'en_US' }['value'] == 'Author'
+              end
+
+              authorships.each_with_index do |a, i|
+                if a['person'].present?
+                  u = User.find_by(pure_uuid: a['person']['uuid'])
+
+                  if u
+                    authorship = Authorship.find_by(user: u, publication: p)
+
+                    unless authorship
+                      Authorship.create!(user: u,
+                                         publication: p,
+                                         author_number: i+1)
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+      pbar.increment unless Rails.env.test?
+    end
+    pbar.finish unless Rails.env.test?
+    nil
+  end
+end
