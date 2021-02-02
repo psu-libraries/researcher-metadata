646171c76bcf3fa08f1ed99740bccbe99e0ccb02
Eric Durante <ericdurante@gettis.local>
2021-02-02 13:05:28 -0500

    Update URL to create a new work in Scholarsphere

--- app/controllers/scholarsphere_uploads_controller.rb
+++ app/controllers/scholarsphere_uploads_controller.rb
@@ -3,6 +3,6 @@ class ScholarsphereUploadsController < OpenAccessWorkflowController
     authorship = Authorship.find_by(user: current_user, publication: publication)
     authorship.update!(scholarsphere_uploaded_at: Time.current,
                        updated_by_owner_at: Time.current)
-    redirect_to 'https://scholarsphere.psu.edu/concern/generic_works/new'
+    redirect_to 'https://scholarsphere.psu.edu/dashboard/form/work_versions/new'
   end
 end
