f8ff7e4998b70c639a5553ec9d8992830aa78244
Alex Kiessling <ajkiessl@gmail.com>
2021-02-11 09:44:18 -0500

    added sql query for oa email to only grab journal articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,6 +91,7 @@ class User < ApplicationRecord
 
   def self.needs_open_access_notification
     joins(:authorships, :publications, :user_organization_memberships).
+    where("publications.publication_type ~* 'Journal Article'").
     where('publications.id NOT IN (SELECT publication_id from authorships WHERE authorships.id IN (SELECT authorship_id from internal_publication_waivers))').
     where('publications.id NOT IN (SELECT publication_id from authorships WHERE scholarsphere_uploaded_at IS NOT NULL)').
     where('users.open_access_notification_sent_at IS NULL OR users.open_access_notification_sent_at < ?', 6.months.ago).
