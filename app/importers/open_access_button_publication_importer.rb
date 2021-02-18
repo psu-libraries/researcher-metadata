adab780b171e5efb784fc27e4cd53708c5d61652
Eric Durante <ericdurante@gettis.local>
2021-02-18 11:24:56 -0500

    Check all publications for OAB import regardless of recent checks
    At least for now, we want to ensure that we have as much of the
    available open access information as possible right before we send out
    open access reminder emails to faculty. We don’t want to send a reminder
    to someone about one of their publications if the publication was just
    recently added to Open Access Button. We may want to toggle this change
    in the future depending on the desired schedule for Open Access Button
    importing in general and prior to email sending.

--- app/importers/open_access_button_publication_importer.rb
+++ app/importers/open_access_button_publication_importer.rb
@@ -22,8 +22,7 @@ class OpenAccessButtonPublicationImporter
   private
 
   def pub_query
-    Publication.where.not(doi: nil).where.not(doi: '').where(open_access_url: nil).
-      where('open_access_button_last_checked_at IS NULL OR open_access_button_last_checked_at < ?', 31.days.ago)
+    Publication.where.not(doi: nil).where.not(doi: '').where(open_access_url: nil)
   end
 
   def get_pub(url)
