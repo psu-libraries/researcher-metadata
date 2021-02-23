15d3b0b287c2802e10e10f811d375c9360b36e59
Eric Durante <ericdurante@gettis.local>
2021-02-23 16:28:08 -0500

    Recover and record error when sending open access reminder email fails

--- app/models/open_access_notifier.rb
+++ app/models/open_access_notifier.rb
@@ -6,16 +6,20 @@ class OpenAccessNotifier
   def send_notifications
     users.each do |u|
       user_profile = UserProfile.new(u)
-      FacultyNotificationsMailer.open_access_reminder(user_profile,
-                                                      u.old_potential_open_access_publications,
-                                                      u.new_potential_open_access_publications).deliver_now
+      begin
+        FacultyNotificationsMailer.open_access_reminder(user_profile,
+                                                        u.old_potential_open_access_publications,
+                                                        u.new_potential_open_access_publications).deliver_now
 
-      ActiveRecord::Base.transaction do
-        u.record_open_access_notification
-        pubs = u.old_potential_open_access_publications + u.new_potential_open_access_publications
-        pubs.each do |p|
-          p.authorships.find_by(user: u).record_open_access_notification
+        ActiveRecord::Base.transaction do
+          u.record_open_access_notification
+          pubs = u.old_potential_open_access_publications + u.new_potential_open_access_publications
+          pubs.each do |p|
+            p.authorships.find_by(user: u).record_open_access_notification
+          end
         end
+      rescue Net::SMTPFatalError => e
+        EmailError.create!(message: e.message, user: u)
       end
     end
   end
