1ba2e6caa1cd6c2c4be72de739d276c5229d291f
Alex Kiessling <ajkiessl@gmail.com>
2021-04-05 10:26:01 -0400

    Limit statistics snapshot to articles only - Changed publications to articles in columns of statistic_snapshot table - Changed the model and rails admin naming to reflect - Fixed specs and testing that non-srticle pubs are not included

--- app/models/statistics_snapshot.rb
+++ app/models/statistics_snapshot.rb
@@ -1,26 +1,26 @@
 class StatisticsSnapshot < ApplicationRecord
 
   def self.record
-    create(total_publication_count: Publication.count,
-           open_access_publication_count: Publication.open_access.count)
+    create(total_article_count: Publication.journal_article.count,
+           open_access_article_count: Publication.journal_article.open_access.count)
   end
 
   def percent_open_access
-    ((open_access_publication_count.to_f / total_publication_count) * 100).round 1
+    ((open_access_article_count.to_f / total_article_count) * 100).round 1
   end
 
   rails_admin do
     list do
       field(:created_at)
-      field(:total_publication_count)
-      field(:open_access_publication_count)
+      field(:total_article_count)
+      field(:open_access_article_count)
       field(:percent_open_access) { label '% open access' }
     end
 
     show do
       field(:created_at)
-      field(:total_publication_count)
-      field(:open_access_publication_count)
+      field(:total_article_count)
+      field(:open_access_article_count)
       field(:percent_open_access)
     end
   end
