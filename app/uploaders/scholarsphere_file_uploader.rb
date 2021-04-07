e73ec4893dda6877727c6be06a05070ae6679d0e
Eric Durante <ericdurante@gettis.local>
2021-04-07 10:40:16 -0400

    Try being more explicit about where carrierwave should store files
    I don’t know why, but it’s still trying to write to the app’s tmp
    directory in deployed environments.

--- app/uploaders/scholarsphere_file_uploader.rb
+++ app/uploaders/scholarsphere_file_uploader.rb
@@ -1,23 +1,19 @@
 class ScholarsphereFileUploader < CarrierWave::Uploader::Base
   storage :file
 
-  def root
-    Rails.root
-  end
-
   def store_dir
     if Rails.env.test?
-      "tmp/uploads/scholarsphere_file_uploads/#{model.id}/file"
+      Rails.root.join "tmp/uploads/scholarsphere_file_uploads/#{model.id}/file"
     else
-      "uploads/scholarsphere_file_uploads/#{model.id}/file"
+      Rails.root.join "uploads/scholarsphere_file_uploads/#{model.id}/file"
     end
   end
 
   def cache_dir
     if Rails.env.test?
-      "tmp/uploads/cache/scholarsphere_file_uploads/#{model.id}/file"
+      Rails.root.join "tmp/uploads/cache/scholarsphere_file_uploads/#{model.id}/file"
     else
-      "uploads/cache/scholarsphere_file_uploads/#{model.id}/file"
+      Rails.root.join "uploads/cache/scholarsphere_file_uploads/#{model.id}/file"
     end
   end
 end
