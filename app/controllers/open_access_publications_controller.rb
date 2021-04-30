4417a2d3feb5f8680ca5ea94dddda1755e36bc74
Eric Durante <eric@westarete.com>
2021-04-30 13:40:25 -0400

    Use newer/preferred update method name

--- app/controllers/open_access_publications_controller.rb
+++ app/controllers/open_access_publications_controller.rb
@@ -17,7 +17,7 @@ class OpenAccessPublicationsController < OpenAccessWorkflowController
     @form = OpenAccessURLForm.new(form_params)
       
     if @form.valid?
-      publication.update_attributes!(user_submitted_open_access_url: @form.open_access_url)
+      publication.update!(user_submitted_open_access_url: @form.open_access_url)
       flash[:notice] = I18n.t('profile.open_access_publications.update.success')
       redirect_to edit_profile_publications_path
     else
