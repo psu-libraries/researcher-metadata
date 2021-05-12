37f99494d66653e1efe8f2abb4ba07e2faa8ca35
Eric Durante <eric@westarete.com>
2021-05-12 19:29:29 -0400

    Only show the admin link on the home page if an admin user is signed in

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -5,9 +5,7 @@ feature "Home page", type: :feature do
 
   shared_examples_for "a page with the public layout" do
 
-    it "shows a link to the admin interface" do
-      expect(page).to have_link 'Admin', href: rails_admin_path
-    end
+
 
     it "shows a link to the profile page" do
       expect(page).to have_link 'Profile', href: profile_bio_path
@@ -18,7 +16,7 @@ feature "Home page", type: :feature do
     end
   end
 
-  context "when the user is logged in" do
+  context "when a regular user is logged in" do
     before do
       authenticate_user
       visit root_path
@@ -38,6 +36,39 @@ feature "Home page", type: :feature do
 
     it "shows a link to the developer resources page" do
       expect(page).to have_link 'Build Profiles', href: resources_path
+    end
+
+    it "does not show a link to the admin interface" do
+      expect(page).not_to have_link 'Admin'
+    end
+
+    it_behaves_like "a page with the public layout"
+  end
+
+  context "when an admin user is logged in" do
+    before do
+      authenticate_admin_user
+      visit root_path
+    end
+
+    it "shows the home page content" do
+      expect(page).to have_content home_content
+    end
+
+    it "shows an email link" do
+      expect(page).to have_link 'Learn More', href: "mailto:L-FAMS@lists.psu.edu?Subject=RMD%20Inquiry"
+    end
+
+    it "shows a link to the API documentation" do
+      expect(page).to have_link 'API Documentation', href: swagger_ui_engine_path
+    end
+
+    it "shows a link to the developer resources page" do
+      expect(page).to have_link 'Build Profiles', href: resources_path
+    end
+
+    it "shows a link to the admin interface" do
+      expect(page).to have_link 'Admin', href: rails_admin_path
     end
 
     it_behaves_like "a page with the public layout"
@@ -60,6 +91,10 @@ feature "Home page", type: :feature do
 
     it "shows a link to the developer resources page" do
       expect(page).to have_link 'Build Profiles', href: resources_path
+    end
+
+    it "does not show a link to the admin interface" do
+      expect(page).not_to have_link 'Admin'
     end
 
     it_behaves_like "a page with the public layout"
