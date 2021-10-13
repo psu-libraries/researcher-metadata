85001e2befc329fe80eb6ed89fa8263ad825f535
Adam Wead <awead@users.noreply.github.com>
2021-10-13 12:03:23 -0400

    Don't display the email if the user is inactive
    If a user's profile is inactive, their Penn State email either no longer
    exists, or they are not actively receiving new email. In that case, we
    don't display their PSU email on their public profile page.

--- app/serializers/api/v1/user_profile_serializer.rb
+++ app/serializers/api/v1/user_profile_serializer.rb
@@ -3,11 +3,15 @@
 module API::V1
   class UserProfileSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :name, :organization_name, :title, :email, :office_location,
+    attributes :name, :organization_name, :title, :office_location,
                :office_phone_number, :personal_website, :total_scopus_citations,
                :scopus_h_index, :pure_profile_url, :orcid_identifier, :bio,
                :teaching_interests, :research_interests, :publications, :other_publications, :grants,
                :presentations, :performances, :master_advising_roles, :phd_advising_roles,
                :news_stories, :education_history
+
+    attribute :email, if: Proc.new { |record|
+      record.active?
+    }
   end
 end
