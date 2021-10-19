6d76a14a67e96250534559d1046183931bcc1a26
Alex Kiessling <ajkiessl@gmail.com>
2021-10-19 16:43:07 -0400

    Adds OpenAccessUrlsMigrationService

---
+++ app/services/open_access_urls_migration_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class OpenAccessUrlsMigrationService
+  def self.call
+    Publication.all.each do |pub|
+      if pub.open_access_url
+        OpenAccessLocation.find_or_create_by(source: 'Open Access Button',
+                                             url: pub.open_access_url,
+                                             publication_id: pub.id)
+      elsif pub.scholarsphere_open_access_url
+        OpenAccessLocation.find_or_create_by(source: 'ScholarSphere',
+                                             url: pub.scholarsphere_open_access_url,
+                                             publication_id: pub.id)
+      elsif pub.user_submitted_open_access_url
+        OpenAccessLocation.find_or_create_by(source: 'User',
+                                             url: pub.user_submitted_open_access_url,
+                                             publication_id: pub.id)
+      end
+    end
+  end
+end
