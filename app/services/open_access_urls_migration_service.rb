90de17481f15f75f475cc7f3d93d72792bb8762b
Alex Kiessling <ajkiessl@gmail.com>
2021-10-20 10:15:14 -0400

    Fixed OA migrations service bug, added rake task to run the service.

--- app/services/open_access_urls_migration_service.rb
+++ app/services/open_access_urls_migration_service.rb
@@ -7,11 +7,13 @@ class OpenAccessUrlsMigrationService
         OpenAccessLocation.find_or_create_by(source: 'Open Access Button',
                                              url: pub.open_access_url,
                                              publication_id: pub.id)
-      elsif pub.scholarsphere_open_access_url
+      end
+      if pub.scholarsphere_open_access_url
         OpenAccessLocation.find_or_create_by(source: 'ScholarSphere',
                                              url: pub.scholarsphere_open_access_url,
                                              publication_id: pub.id)
-      elsif pub.user_submitted_open_access_url
+      end
+      if pub.user_submitted_open_access_url
         OpenAccessLocation.find_or_create_by(source: 'User',
                                              url: pub.user_submitted_open_access_url,
                                              publication_id: pub.id)
