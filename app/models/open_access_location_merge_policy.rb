55cb2f24b702b2d086850f6be2fdfc11f34cab2b
Alex Kiessling <ajkiessl@gmail.com>
2021-10-25 10:58:55 -0400

    Moved OAL merge logic to its own class

---
+++ app/models/open_access_location_merge_policy.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class OpenAccessLocationMergePolicy
+  def initialize(publications)
+    @publications = publications
+  end
+
+  def open_access_locations_to_keep
+    publications.map(&:open_access_locations).flatten.uniq { |oal| [oal.source, oal.url] }
+  end
+end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -571,9 +571,9 @@ class Publication < ApplicationRecord
         amp.waivers_to_destroy.each(&:destroy)
       end
 
-      new_oa_locations = all_pubs.map(&:open_access_locations).flatten.uniq { |oal| [oal.source, oal.url] }
+      oalmp = OpenAccessLocationMergePolicy.new(all_pubs)
       open_access_locations = []
-      open_access_locations << new_oa_locations
+      open_access_locations << oalmp.open_access_locations_to_keep
 
       pubs_to_delete.each do |p|
         p.non_duplicate_groups.each do |ndg|
