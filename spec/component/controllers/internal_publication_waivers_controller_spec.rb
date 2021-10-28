cc34893499fc82c45351a02d510652dc2060b534
Ryan Schenk <rschenk@gmail.com>
2021-10-28 10:53:43 -0400

    Fix internal_publication_waivers_controller_spec

--- spec/component/controllers/internal_publication_waivers_controller_spec.rb
+++ spec/component/controllers/internal_publication_waivers_controller_spec.rb
@@ -6,8 +6,8 @@ describe InternalPublicationWaiversController, type: :controller do
   let!(:user) { create :user }
   let!(:other_user) { create :user }
   let!(:pub) { create :publication }
-  let!(:oa_pub) { create :publication, open_access_url: 'url' }
-  let!(:uoa_pub) { create :publication, user_submitted_open_access_url: 'url' }
+  let!(:oa_pub) { create :publication, open_access_locations: [build(:open_access_location, :open_access_button)] }
+  let!(:uoa_pub) { create :publication, open_access_locations: [build(:open_access_location, :user)] }
   let!(:other_pub) { create :publication }
   let!(:uploaded_pub) { create :publication }
   let!(:other_uploaded_pub) { create :publication }
