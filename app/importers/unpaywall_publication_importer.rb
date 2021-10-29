dae3ea22c2cff5d1a4bd5e5d6b669c52cd1915b9
Nick Tiberi <nicktiberi@gmail.com>
2021-10-29 10:12:22 -0400

    Unpaywall importer: import all OA locations + more metadata (#318)
    * Unpaywall importer: import all OA locations + more metadata
    
    We now pull all OA locations in the Unpaywall import process,
    not just those marked as "is_best". We also pull more metadata
    fields from each OA location in Unpaywall.
    
    This import will keep the Unpaywall OA locations in RMD in sync
    with the data coming from Unpaywall:
    
    - If an OA URL in RMD no longer exists in Unpaywall, the OA location
      in RMD  will be deleted.
    - If an OA URL in RMD matches one from Unpaywall, we will update the
      OA location in RMD.
    - If an OA URL coming from Unpaywall is not found in RMD, we will
      create a new OA location in RMD with that URL.
    
    Co-authored-by: Ryan Schenk <rschenk@gmail.com>

--- app/importers/unpaywall_publication_importer.rb
+++ app/importers/unpaywall_publication_importer.rb
@@ -6,7 +6,7 @@ class UnpaywallPublicationImporter
                               total: all_pubs.count)
 
     all_pubs.find_each do |p|
-      query_unpaywall_for(p)
+      import_from_unpaywall(p)
       pbar.increment
     end
     pbar.finish
@@ -17,7 +17,7 @@ class UnpaywallPublicationImporter
                               total: new_pubs.count)
 
     new_pubs.find_each do |p|
-      query_unpaywall_for(p)
+      import_from_unpaywall(p)
       pbar.increment
     end
     pbar.finish
@@ -26,35 +26,16 @@ class UnpaywallPublicationImporter
   private
 
     def all_pubs
-      Publication.where.not(doi: nil).where.not(doi: '')
+      Publication.where.not(doi: nil).where.not(doi: '').includes(:open_access_locations)
     end
 
     def new_pubs
       all_pubs.where(unpaywall_last_checked_at: nil)
     end
 
-    def query_unpaywall_for(publication)
-      unpaywall_json = nil
-      find_url = URI::DEFAULT_PARSER.escape("https://api.unpaywall.org/v2/#{publication.doi_url_path}?email=openaccess@psu.edu")
-      unpaywall_json = JSON.parse(HttpService.get(find_url))
-
-      best_oa_location = unpaywall_json.dig('best_oa_location', 'url')
-      existing_oa_location = publication.open_access_locations.find_by(source: Source::UNPAYWALL)
-      oa_status = unpaywall_json['oa_status']
-
-      if best_oa_location
-        if existing_oa_location
-          existing_oa_location.update!(url: best_oa_location)
-        else
-          publication.open_access_locations.create!(source: Source::UNPAYWALL, url: best_oa_location)
-        end
-      else
-        existing_oa_location.try(:destroy)
-      end
-
-      publication.open_access_status = oa_status
-      publication.unpaywall_last_checked_at = Time.zone.now
-      publication.save!
+    def import_from_unpaywall(publication)
+      unpaywall_json = query_unpaywall_for(publication)
+      update_publication(publication, unpaywall_json)
 
       # Unpaywall asks that users limit requests to no more than 100,000 per day.
       # Limiting to 1 request per second caps us at 86,400 requests per day.
@@ -69,5 +50,63 @@ class UnpaywallPublicationImporter
           unpaywall_json: unpaywall_json.to_s
         }
       )
+    end
+
+    def update_publication(publication, unpaywall_json)
+      unpaywall_locations = unpaywall_json['oa_locations'].presence || []
+      existing_locations = publication.open_access_locations.filter { |l| l.source == Source::UNPAYWALL }
+
+      existing_locations_by_url = existing_locations.index_by(&:url)
+      unpaywall_locations_by_url = unpaywall_locations.index_by { |l| l['url'] }
+
+      ActiveRecord::Base.transaction do
+        unpaywall_locations.each do |unpaywall_location_data|
+          unpaywall_url = unpaywall_location_data['url']
+          open_access_location = existing_locations_by_url.fetch(unpaywall_url) { build_new_oal(publication, unpaywall_url) }
+
+          update_open_access_location(open_access_location, unpaywall_location_data)
+          open_access_location.save!
+        rescue StandardError => e
+          ImporterErrorLog.log_error(
+            importer_class: self.class,
+            error: e,
+            metadata: {
+              publication_id: publication&.id,
+              publication_doi_url_path: publication&.doi_url_path,
+              unpaywall_json: unpaywall_location_data
+            }
+          )
+        end
+
+        locations_to_delete = existing_locations.reject { |l| unpaywall_locations_by_url.key? l.url }
+        locations_to_delete.each(&:destroy)
+
+        publication.open_access_status = unpaywall_json['oa_status']
+        publication.unpaywall_last_checked_at = Time.zone.now
+
+        publication.save!
+      end
+    end
+
+    def query_unpaywall_for(publication)
+      find_url = URI::DEFAULT_PARSER.escape("https://api.unpaywall.org/v2/#{publication.doi_url_path}?email=openaccess@psu.edu")
+      JSON.parse(HttpService.get(find_url))
+    end
+
+    def update_open_access_location(open_access_location, unpaywall_json)
+      open_access_location.assign_attributes(
+        landing_page_url: unpaywall_json['url_for_landing_page'],
+        pdf_url: unpaywall_json['url_for_pdf'],
+        host_type: unpaywall_json['host_type'],
+        is_best: unpaywall_json['is_best'],
+        license: unpaywall_json['license'],
+        oa_date: unpaywall_json['oa_date'],
+        source_updated_at: unpaywall_json['updated'],
+        version: unpaywall_json['version']
+      )
+    end
+
+    def build_new_oal(publication, url)
+      publication.open_access_locations.build(source: Source::UNPAYWALL, url: url)
     end
 end
