9672687a81c6207c5449cdd7becddfcf5920687d
Adam Wead <awead@users.noreply.github.com>
2021-11-09 09:28:25 -0500

    Allow hosts for development and testing
    Allows any hostname that we might be using locally. This bypasses
    certain security measures than only need to be used in production.
    Hostname checks for test are disabled completely, allowing any named
    host. This is typically "www.example.com" by default, but it doesn't
    need to be tested.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,6 +10,9 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Disable hostname checks
+  config.hosts.clear
+
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
