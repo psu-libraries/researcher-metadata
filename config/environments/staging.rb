122dac15e7a83fb0b1d1a1470a2d7e23e12781b7
Alex Kiessling <ajkiessl@gmail.com>
2021-11-23 07:33:54 -0500

    Adds host to staging mailer config.  Even though mailers are disabled, this will cause an error if not defined.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'metadata-qa.libraries.psu.edu' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
