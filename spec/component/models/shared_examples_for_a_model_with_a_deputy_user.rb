3d6838125ca5dbd5e4969c02f14560f407e5fc55
Alex Kiessling <ajkiessl@gmail.com>
2021-12-01 11:43:09 -0500

    Adds a test

--- spec/component/models/shared_examples_for_a_model_with_a_deputy_user.rb
+++ spec/component/models/shared_examples_for_a_model_with_a_deputy_user.rb
@@ -3,9 +3,15 @@
 shared_examples_for 'a model with a deputy user' do
   subject(:example) { described_class.new }
 
-  context 'when setting the deputy with user' do
+  context 'when setting the deputy with user that is a deputy' do
     let(:user) { create(:user, primaries: [primary_user]) }
     let(:primary_user) { create(:user) }
+
+    it { is_expected.to allow_value(user).for(:deputy) }
+  end
+
+  context 'when setting the deputy with user that is not a deputy' do
+    let(:user) { create(:user) }
 
     it { is_expected.to allow_value(user).for(:deputy) }
   end
