75bf29ccf8702c2cfe1a006eb7b0fe429c0d0dc0
Alex Kiessling <ajkiessl@gmail.com>
2022-08-08 16:02:29 -0400

    Using double splat instead of hash argument

--- app/components/deputy_assignment_component.rb
+++ app/components/deputy_assignment_component.rb
@@ -43,7 +43,7 @@ class DeputyAssignmentComponent < ViewComponent::Base
   end
 
   def delete_confirm
-    i18n('delete_confirmation')
+    i18n('delete_confirmation', action: delete_text.downcase)
   end
 
   def delete_class
@@ -61,7 +61,7 @@ class DeputyAssignmentComponent < ViewComponent::Base
     attr_reader :deputy_assignment,
                 :current_user
 
-    def i18n(key)
-      I18n.t("view_component.#{self.class.name.underscore}.#{key}")
+    def i18n(key, **options)
+      I18n.t("view_component.#{self.class.name.underscore}.#{key}", **options)
     end
 end
