e829861ff61302c070d104fd0fa5b009abba0444
whereismyjetpack <djb44@psu.edu>
2022-08-31 12:24:06 -0400

    adds datadog and datadog configuration
    Co-authored-by: Adam Michaels <58698717+off2seethewizard@users.noreply.github.com>

---
+++ config/initializers/datadog.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.configuration.x.datadog = Rails.application.config_for(:datadog)
+
+if Rails.configuration.x.datadog[:enabled]
+  require 'ddtrace'
+  Datadog.configure do |c|
+    c.tracing.instrument :faraday, service_name: 'researcher-metadata-faraday'
+    c.env = Rails.configuration.x.datadog[:env] || 'dev'
+  end
+end
