89cb515c87c0a57f795febe5f2d40f6d0a40ada4
whereismyjetpack <djb44@psu.edu>
2022-09-01 14:36:15 -0400

    try this

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,7 +3,6 @@
 Rails.configuration.x.datadog = Rails.application.config_for(:datadog)
 
 if Rails.configuration.x.datadog[:enabled]
-  require 'ddtrace/auto_instrument'
   Datadog.configure do |c|
     c.tracing.instrument :faraday, service_name: 'researcher-metadata-faraday'
     c.env = Rails.configuration.x.datadog[:env] || 'dev'
