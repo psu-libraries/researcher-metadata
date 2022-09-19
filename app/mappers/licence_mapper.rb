2290fbaf0ac2bab5fc158cb826a2ddf39051a0f8
Alex Kiessling <ajkiessl@gmail.com>
2022-09-19 14:31:52 -0400

    Licence mapper and tests

---
+++ app/mappers/licence_mapper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class LicenceMapper
+  def self.map(string)
+    case string.downcase
+    when 'cc-by', 'cc-by 3.0', 'cc-by 4.0'
+      ScholarsphereWorkDeposit.rights[0]
+    when 'cc-by-nc 4.0'
+      ScholarsphereWorkDeposit.rights[2]
+    when 'cc-by-nc-nd'
+      ScholarsphereWorkDeposit.rights[4]
+    when 'cc-by-nc-sa'
+      ScholarsphereWorkDeposit.rights[5]
+    when 'cc0'
+      ScholarsphereWorkDeposit.rights[7]
+    else
+      ScholarsphereWorkDeposit.rights[8]
+    end
+  end
+end
