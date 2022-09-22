f53a983fbc513a8842b1347c247f1e17670f5e72
Banu Kutlu <bzk60@psu.edu>
2022-09-22 09:27:13 -0400

    WIP trying to cache temp files

--- app/models/exif_uploads.rb
+++ app/models/exif_uploads.rb
@@ -13,6 +13,12 @@ class ExifUploads
   end
 
   def self.temp_files(uploads)
+    # TODO: check if these can be moved to cache so we can retrieve later
+    # uploader = ScholarsphereFileUploader.new
+    # uploads.values.each do |upload|
+    #   uploader.store!(upload['file'])
+    # end
+    # uploader.retrieve_from_store!(uploads.values.first['file'].original_filename)
     uploads.values.map { |upload| { original_filename: upload['file'].original_filename, temp_path: upload['file'].path } }
   end
 end
