ba7146436a5c6faea4bcb39090b5c94835c9dc06
whereismyjetpack <djb44@psu.edu>
2023-01-12 16:34:41 -0500

    configures json logging

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  config.lograge.enabled = Settings.lograge.enabled
+  config.lograge.formatter = Lograge::Formatters::Logstash.new if Settings.lograge.json
+
+  config.lograge.custom_payload do |controller|
+    {
+      request_id: controller.request.env['action_dispatch.request_id']
+    }
+  end
+
+  config.lograge.custom_options = lambda do |event|
+    {
+      remote_addr: event.payload[:headers][:REMOTE_ADDR],
+      x_forwarded_for: event.payload[:headers][:HTTP_X_FORWARDED_FOR]
+    }
+  end
+end
