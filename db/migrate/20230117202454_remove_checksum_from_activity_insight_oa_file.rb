5726fe2a34a792704c5eb27d6e9be59b4bdb1e18
Alex Kiessling <ajkiessl@gmail.com>
2023-01-17 15:30:55 -0500

    Remove checksum from ActivityInsightOaFile

---
+++ db/migrate/20230117202454_remove_checksum_from_activity_insight_oa_file.rb
@@ -0,0 +1,9 @@
+class RemoveChecksumFromActivityInsightOaFile < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :activity_insight_oa_files, :checksum, :string
+  end
+
+  def down
+    add_column :activity_insight_oa_files, :checksum, :string
+  end
+end
--- spec/component/models/activity_insight_oa_file_spec.rb
+++ spec/component/models/activity_insight_oa_file_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe ActivityInsightOaFile, type: :model do
   subject { described_class.new }
 
   it { is_expected.to have_db_column(:location).of_type(:string) }
-  it { is_expected.to have_db_column(:checksum).of_type(:string) }
   it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
   it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
 
