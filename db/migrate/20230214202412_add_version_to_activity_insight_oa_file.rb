7328b8174acd5579bd2460f44c4e3824a1864c21
Smulligan <smm5878@psu.edu>
2023-02-14 15:34:49 -0500

    refactor permissions job to handle file version

---
+++ db/migrate/20230214202412_add_version_to_activity_insight_oa_file.rb
@@ -0,0 +1,5 @@
+class AddVersionToActivityInsightOaFile < ActiveRecord::Migration[6.1]
+  def change
+    add_column :activity_insight_oa_files, :version, :string
+  end
+end
--- spec/component/jobs/permissions_check_job_spec.rb
+++ spec/component/jobs/permissions_check_job_spec.rb
@@ -6,15 +6,15 @@ describe PermissionsCheckJob, type: :job do
   describe '#perform' do
     let!(:publication) { create(:publication,
                                 doi: 'https://doi.org/10.1016/S0962-1849(05)80014-9')}
-    let!(:activity_insight_oa_file1) { create(:activity_insight_oa_file, publication: publication) }
-    let!(:activity_insight_oa_file2) { create(:activity_insight_oa_file, publication: publication) }
+    let!(:activity_insight_oa_file1) { create(:activity_insight_oa_file, publication: publication, version: 'accepted') }
+    let!(:activity_insight_oa_file2) { create(:activity_insight_oa_file, publication: publication, version: 'published') }
     let!(:activity_insight_oa_file3) { create(:activity_insight_oa_file, publication: publication) }
     let(:job) { described_class.new }
 
     context 'when the preferred version is the correct version' do
       it 'updates the publication permissions' do
         job.perform(publication)
-        expect(publication.reload.permissions_last_checked_at).not_to be_nil
+        expect(publication.reload.permissions_last_checked_at).to be_within(1.minute).of(Time.zone.now)
         expect(publication.reload.set_statement).to eq '© This manuscript version is made available under the CC-BY-NC-ND 4.0 license https://creativecommons.org/licenses/by-nc-nd/4.0/'
         expect(publication.reload.licence).to eq 'https://creativecommons.org/licenses/by-nc-nd/4.0/'
       end
@@ -25,7 +25,7 @@ describe PermissionsCheckJob, type: :job do
                                    doi: nil)}
       it 'does not update the publication permissions' do
         job.perform(publication)
-        expect(publication.reload.permissions_last_checked_at).not_to be_nil
+        expect(publication.reload.permissions_last_checked_at).to be_within(1.minute).of(Time.zone.now)
         expect(publication.reload.set_statement).to be_nil
         expect(publication.reload.licence).to be_nil
       end
