48fe9bd5b3777afd5e851df6c5aa7bde0c7e062a
Eric Durante <eric@westarete.com>
2023-02-17 12:04:38 -0500

    Tell zeitwerk to ignore files that don’t define constants

---
+++ config/initializers/zeitwerk.rb
@@ -0,0 +1,6 @@
+Rails.autoloaders.main.ignore(
+  Rails.root.join("app/rails_admin_actions"),
+  Rails.root.join("app/assets"),
+  Rails.root.join("app/javascripts"),
+  Rails.root.join("app/views")
+)
