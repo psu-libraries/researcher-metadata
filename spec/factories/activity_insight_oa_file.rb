305f0ddabcfbcb56fb3fc179811faa0aeb26a6ad
Alex Kiessling <ajkiessl@gmail.com>
2023-02-27 12:20:18 -0500

    Tests for #version_status_display

--- spec/factories/activity_insight_oa_file.rb
+++ spec/factories/activity_insight_oa_file.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :activity_insight_oa_file do
     publication
     location { 'abc123/intellcont/test_file.pdf' }
-    version { I18n.t('file_versions.accepted_version') }
+    version { nil }
   end
 end
