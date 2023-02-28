65deca7958f7767a14c820a7e33decd1ebabc9d4
Alex Kiessling <ajkiessl@gmail.com>
2023-02-28 11:11:21 -0500

    Fixed spec

--- spec/integration/admin/activity_insight_oa_workflow/file_version_review_spec.rb
+++ spec/integration/admin/activity_insight_oa_workflow/file_version_review_spec.rb
@@ -2,10 +2,11 @@
 
 require 'integration/integration_spec_helper'
 
-describe 'Admin File Version Curation dashboard', type: :feature do
+describe 'Admin File Version Review dashboard', type: :feature do
   let!(:aif1) { create(:activity_insight_oa_file, publication: pub1, version: 'unknown') }
   let!(:aif2) { create(:activity_insight_oa_file, publication: pub1, version: 'unknown') }
   let!(:aif3) { create(:activity_insight_oa_file, publication: pub2, version: 'publishedVersion') }
+  let!(:aif4) { create(:activity_insight_oa_file, publication: pub2, version: 'unknown') }
   let!(:pub1) { create(:publication, preferred_version: 'acceptedVersion') }
   let!(:pub2) { create(:publication, preferred_version: 'acceptedVersion') }
 
@@ -22,7 +23,7 @@ describe 'Admin File Version Curation dashboard', type: :feature do
       expect(page).to have_text('Version Status')
       expect(page).to have_text(pub1.title)
       expect(page).to have_text(pub2.title)
-      expect(find_all('td[rowspan="2"]').count).to eq 2
+      expect(find_all('td[rowspan="2"]').count).to eq 4
       expect(find_all('td[rowspan="2"]').first.text).to eq pub1.title
       expect(page).to have_text('Accepted Manuscript').twice
       expect(page).to have_text(pub1.activity_insight_oa_files.first.location)
