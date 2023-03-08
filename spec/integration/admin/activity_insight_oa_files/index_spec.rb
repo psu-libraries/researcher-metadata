005a07403b1226fdbe9da7b84bc4e2c9be9e2ae4
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2023-03-08 12:39:15 -0500

    Editing activity insight oa files (#725)
    * Got the scope for this working
    
    * Components for Unkown Version List UI added and working: Views, ViewComponents, controller, route.
    
    * Started on tests for scope
    
    * Got the scope to work
    
    * Refined the sql
    
    * Removes placeholder db migration.  The scope for publications that will show up in this list is correct now.
    
    * Nested rows in the failed file verifictation list.  We'll need this for publications with multiple files.
    
    * Display 'Version Status' and 'Preferred Version' in list.  Model Helper methods added to display these properly.
    
    * Renamed 'unknown_version' file/class/method naming to 'file_version_check'
    
    * Tests for file_version_check_failed scope.
    
    * Test for #preferred_version_display
    
    * Tests for #version_status_display
    
    * View component tests for the file version check card on the activity insight oa dashboard
    
    * Niftany
    
    * Renamed some things.  Finished up integration specs
    
    * Test rowspan
    
    * Title change
    
    * Quotes aroung attribute value for rowspan
    
    * Fixed test.  Adds capybara to component spec helper
    
    * Changes scope to exclude any publications that have all versions determined, but no correct version.  Modified tests to test this.  Changed some wording for this list's description
    
    * Fixed spec
    
    * Niftany
    
    * adds rails admin show, list, and edit to ActivityInsightOAFiles
    
    * Some integration tests for editing activity insight oa files
    
    * Validate version and test
    
    * Adds some tests
    
    * Niftany

---
+++ spec/integration/admin/activity_insight_oa_files/index_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'integration/integration_spec_helper'
+require 'integration/admin/shared_examples_for_admin_page'
+
+describe 'Admin activity insight oa file list', type: :feature do
+  context 'when the current user is an admin' do
+    before { authenticate_admin_user }
+
+    describe 'the page content' do
+      let!(:pub1) { create(:publication, title: 'Test Publication') }
+      let!(:pub2) { create(:publication, title: 'Another Publication') }
+      let!(:aif1) do
+        create(:activity_insight_oa_file,
+               publication: pub1,
+               version: 'acceptedVersion')
+      end
+      let!(:aif2) do
+        create(:activity_insight_oa_file,
+               publication: pub2,
+               version: nil)
+      end
+
+      before { visit rails_admin.index_path(model_name: :activity_insight_oa_file) }
+
+      it 'shows the activity insight oa file list heading' do
+        expect(page).to have_content 'List of Activity insight OA files'
+      end
+
+      it 'shows information about each file' do
+        expect(page).to have_link pub1.title
+        expect(page).to have_content aif1.location
+        expect(page).to have_content 'acceptedVersion'
+
+        expect(page).to have_link pub2.title
+        expect(page).to have_content aif2.location
+      end
+    end
+
+    describe 'the page layout' do
+      before { visit rails_admin.index_path(model_name: :activity_insight_oa_file) }
+
+      it_behaves_like 'a page with the admin layout'
+    end
+  end
+
+  context 'when the current user is not an admin' do
+    before { authenticate_user }
+
+    it 'redirects back to the home page with an error message' do
+      visit rails_admin.index_path(model_name: :activity_insight_oa_file)
+      expect(page).to have_current_path root_path, ignore_query: true
+      expect(page).to have_content I18n.t('admin.authorization.not_authorized')
+    end
+  end
+end
