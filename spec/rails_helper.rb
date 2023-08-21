d32be22c00c2c9ea82c6249b0efb8434f480cfb4
Alex Kiessling <ajkiessl@gmail.com>
2023-08-21 11:43:26 -0400

    See if this works in CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+# Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -59,5 +59,4 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include RailsAdmin::Engine.routes.url_helpers, type: :controller
-  config.include CachingHelpers
 end
