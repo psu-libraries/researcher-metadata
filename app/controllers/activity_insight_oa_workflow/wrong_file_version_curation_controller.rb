6790ee7d25f5dd4dc3d68360b0fd375a06630ac1
Smulligan <smm5878@psu.edu>
2023-08-25 08:01:20 -0400

    sorts wrong file version by email_last_sent_at

--- app/controllers/activity_insight_oa_workflow/wrong_file_version_curation_controller.rb
+++ app/controllers/activity_insight_oa_workflow/wrong_file_version_curation_controller.rb
@@ -2,7 +2,7 @@
 
 class ActivityInsightOAWorkflow::WrongFileVersionCurationController < ActivityInsightOAWorkflowController
   def index
-    @publications = Publication.wrong_file_version
+    @publications = Publication.wrong_file_version.order('email_last_sent_at DESC NULLS FIRST')
   end
 end
   
