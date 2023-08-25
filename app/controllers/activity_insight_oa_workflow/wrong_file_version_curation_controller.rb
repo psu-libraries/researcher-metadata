ef824c805270aa77e0b56efc02777f60ef85187a
Smulligan <smm5878@psu.edu>
2023-08-25 16:38:48 -0400

    update email_last_sent_at timestamp after sending emails

--- app/controllers/activity_insight_oa_workflow/wrong_file_version_curation_controller.rb
+++ app/controllers/activity_insight_oa_workflow/wrong_file_version_curation_controller.rb
@@ -7,6 +7,10 @@ class ActivityInsightOAWorkflow::WrongFileVersionCurationController < ActivityIn
 
   def email_author(publications)
     FacultyNotificationsMailer.wrong_file_version(publications).deliver_now
+    publications.each do |pub|
+      pub.email_last_sent_at = DateTime.current
+      pub.save!
+    end
   end
 end
   
