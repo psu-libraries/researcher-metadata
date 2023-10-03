f4b95095e2654ec1aa7d4241d0bf2363d59e870d
Alex Kiessling <ajkiessl@gmail.com>
2023-10-03 10:28:09 -0400

    Lets see if this works

--- spec/integration/admin/activity_insight_oa_workflow/wrong_file_version_review_spec.rb
+++ spec/integration/admin/activity_insight_oa_workflow/wrong_file_version_review_spec.rb
@@ -69,7 +69,7 @@ describe 'Admin File Version Review dashboard', type: :feature do
 
   describe 'clicking the button to send a single email notification' do
     it 'sends an email and displays a confirmation message' do
-      click_button('Send Email', match: :first)
+      find_all("input[value='Send Email']").first.click
       expect(page).to have_current_path activity_insight_oa_workflow_wrong_file_version_review_path
       expect(page).to have_content('Email sent to abc123')
       open_email('abc123@psu.edu')
