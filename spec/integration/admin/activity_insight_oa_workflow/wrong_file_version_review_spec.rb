24d6645e2ec5c0f0cf7cf4fbc7019689c35fad91
Alex Kiessling <ajkiessl@gmail.com>
2023-10-06 14:50:02 -0400

    Rubocop

--- spec/integration/admin/activity_insight_oa_workflow/wrong_file_version_review_spec.rb
+++ spec/integration/admin/activity_insight_oa_workflow/wrong_file_version_review_spec.rb
@@ -69,7 +69,7 @@ describe 'Admin File Version Review dashboard', type: :feature do
 
   describe 'clicking the button to send a single email notification' do
     it 'sends an email and displays a confirmation message' do
-      find_all("input[name='publications']", visible: false).collect{|i| i if i.value == pub1.id.to_s}.compact.first.sibling("input").click
+      find_all("input[name='publications']", visible: false).select { |i| i.value == pub1.id.to_s }.first.sibling('input').click
       expect(page).to have_current_path activity_insight_oa_workflow_wrong_file_version_review_path
       expect(page).to have_content('Email sent to abc123')
       open_email('abc123@psu.edu')
