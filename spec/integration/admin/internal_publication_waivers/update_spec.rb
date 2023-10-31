f1cc3e849d32078e7eb7fb10a4465f13c5dcd81e
Alex Kiessling <ajkiessl@gmail.com>
2023-10-31 12:33:45 -0400

    Made internal waiver fields read only to fix issue

--- spec/integration/admin/internal_publication_waivers/update_spec.rb
+++ spec/integration/admin/internal_publication_waivers/update_spec.rb
@@ -9,36 +9,21 @@ describe 'updating an internal publication waiver via the admin interface', type
                          reason_for_waiver: 'because') }
   let!(:authorship) { create(:authorship, user: user, publication: pub) }
   let!(:user) { create(:user, first_name: 'James', last_name: 'Testington') }
-  let!(:other_user) { create(:user, first_name: 'Other', last_name: 'User') }
   let!(:pub) { create(:publication, title: 'A Test Publication') }
-  let!(:other_pub) { create(:publication, title: 'Other Pub') }
 
-  context 'when the current user is an admin', js: true do
+  context 'when the current user is an admin' do
     before do
       authenticate_admin_user
       visit rails_admin.edit_path(model_name: :internal_publication_waiver, id: waiver.id)
     end
 
-    describe 'using the edit page' do
-      it 'has link to edit and new modal forms for user associated with the waiver and dropdown of users' do
-        click_link 'Edit this User'
-        sleep 1
-        expect(page).to have_content "Edit User 'James Testington'"
-        fill_in 'Middle name', with: 'Josh'
-        click_link 'Save'
-        click_button 'Save and edit'
-        expect(page).to have_content 'James Josh Testington'
-        expect(page).not_to have_content 'Other User'
-        find_all('label[title="Show All Items"]').first.click
-        expect(page).to have_content 'Other User'
-        click_link 'Add a new User'
-        expect(page).to have_content 'New User'
+    describe 'viewing the edit page' do
+      it 'shows a link to the user associated with the waiver' do
+        expect(page).to have_link 'James Testington', href: rails_admin.show_path(model_name: :user, id: user.id)
       end
 
-      it 'has link to edit and new modal forms for the publication associated with the waiver and dropdown of publications' do
-        click_link 'Edit this Publication'
-        sleep 0.5
-        expect(page).to have_content "Edit Publication 'A Test Publication'"
+      it 'shows a link to the publication associated with the waiver' do
+        expect(page).to have_link 'A Test Publication', href: rails_admin.show_path(model_name: :publication, id: pub.id)
       end
     end
 
