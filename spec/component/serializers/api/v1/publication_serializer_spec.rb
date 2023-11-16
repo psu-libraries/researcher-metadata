98af88e0b2d3ab4a3c262cf8bf0e7d7e7bb0c0d9
Alex Kiessling <ajkiessl@gmail.com>
2023-11-16 13:39:08 -0500

    Moved expect statement to follow same order in let sequence

--- spec/component/serializers/api/v1/publication_serializer_spec.rb
+++ spec/component/serializers/api/v1/publication_serializer_spec.rb
@@ -46,8 +46,8 @@ describe API::V1::PublicationSerializer do
     it { is_expected.to include(authors_et_al: true) }
     it { is_expected.to include(abstract: 'an abstract') }
     it { is_expected.to include(citation_count: 1000) }
-    it { is_expected.to include(doi: 'https://doi.org/10.000/example') }
     it { is_expected.to include(supplementary_url: 'https://example.org/article') }
+    it { is_expected.to include(doi: 'https://doi.org/10.000/example') }
     it { is_expected.to include(preferred_open_access_url: 'OA URL') }
 
     context 'when the publication has a published_on date' do
