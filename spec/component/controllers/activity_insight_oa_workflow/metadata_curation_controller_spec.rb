f325e6b5ae7a05ad28d176fcbb0c9ba3f87b4ead
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2024-02-02 14:47:31 -0500

    Prevent double-clicks (#941)
    * Redirect to metadata list if cannot deposit and show flash warning.  Test
    
    * rubocop
    
    * Disable button after click.  Chagned flash from warning to alert
    
    * Only grab publications that have metadata ready in the create controller.  This will prevent other validation issues.  Catch no record error and redirect.  Test for this
    
    * Revert wording change

--- spec/component/controllers/activity_insight_oa_workflow/metadata_curation_controller_spec.rb
+++ spec/component/controllers/activity_insight_oa_workflow/metadata_curation_controller_spec.rb
@@ -5,19 +5,48 @@ require 'component/component_spec_helper'
 describe ActivityInsightOAWorkflow::MetadataCurationController, type: :controller do
   describe '#create_scholarsphere_deposit' do
     let!(:user) { create(:user, is_admin: true) }
+    let!(:author) { create(:user) }
     let!(:publication) { create(:sample_publication, :oa_publication, preferred_version: 'acceptedVersion') }
-    let!(:ai_oa_file) { create(:activity_insight_oa_file, publication: publication, version: 'acceptedVersion') }
+    let!(:auth) { create(:authorship, publication: publication, user: author) }
+    let!(:ai_oa_file) {
+      create(
+        :activity_insight_oa_file,
+        publication: publication,
+        version: 'acceptedVersion',
+        license: 'https://creativecommons.org/licenses/by/4.0/',
+        set_statement: 'statement',
+        embargo_date: Date.today,
+        downloaded: true,
+        file_download_location: fixture_file_open('test_file.pdf'),
+        user: author
+      )
+    }
 
-    context 'when publications cannot be deposited to scholarsphere' do
+    before do
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    context 'when publication cannot be deposited to scholarsphere' do
       before do
-        allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-        allow(controller).to receive(:current_user).and_return(user)
-        allow(publication).to receive(:scholarsphere_upload_pending?).and_return(true)
+        publication.update abstract: nil
       end
 
       it 'redirects to metadata review list' do
         post :create_scholarsphere_deposit, params: { publication_id: publication.id }
-        expect(flash[:warning]).to eq 'This publication cannot be deposited.'
+        expect(flash[:alert]).to eq 'This publication cannot be deposited.'
+        expect(response.redirect_url).to include activity_insight_oa_workflow_metadata_review_path
+      end
+    end
+
+    context 'when publication is not ready for metadata review' do
+      before do
+        publication.update preferred_version: nil
+      end
+
+      it 'redirects to metadata review list' do
+        post :create_scholarsphere_deposit, params: { publication_id: publication.id }
+        expect(flash[:notice]).to eq 'This publication is not ready for metadata review.'
         expect(response.redirect_url).to include activity_insight_oa_workflow_metadata_review_path
       end
     end
