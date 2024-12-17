1d00795303cb68fc7faafe7a43cb95906ae85130
Jesse Landis-Eigsti <jml8735@psu.edu>
2024-12-17 09:41:02 -0700

    735 - Additional refactors

--- lib/mailer_previews/faculty_notifications_mailer_preview.rb
+++ lib/mailer_previews/faculty_notifications_mailer_preview.rb
@@ -8,10 +8,8 @@ class FacultyNotificationsMailerPreview < ActionMailer::Preview
     pub1 = OpenStruct.new({ title: 'Example Publication One' })
     pub2 = OpenStruct.new({ title: 'Example Publication Two' })
     pub3 = OpenStruct.new({ title: 'Example Publication Three' })
-    old_fake_pubs = [pub1, pub2]
-    new_fake_pubs = [pub3]
 
-    FacultyNotificationsMailer.open_access_reminder(fake_user, old_fake_pubs + new_fake_pubs)
+    FacultyNotificationsMailer.open_access_reminder(fake_user, [pub1, pub2, pub3])
   end
 
   # Accessible from http://localhost:3000/rails/mailers/faculty_notifications_mailer/wrong_file_version
