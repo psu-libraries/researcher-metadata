5ffc95b69cae26967d348b05d40a1fe3f115df90
Eric Durante <eric@westarete.com>
2025-01-23 12:40:00 -0500

    Fix bug in publication DOI merge policy

--- spec/component/models/doi_verification_merge_policy_spec.rb
+++ spec/component/models/doi_verification_merge_policy_spec.rb
@@ -149,6 +149,182 @@ describe DOIVerificationMergePolicy do
         end
       end
 
+      context 'when given a publication to be merged that has a verified DOI that varies from the merge target DOI only by upper/lower case' do
+        let(:pub2) { create(:publication, doi_verified: true, doi: 'https://doi.org/10.1103/PHYSREVLETT.80.3915') }
+
+        context 'when the set of given publications to be merged includes the merge target' do
+          let(:publications_to_merge) { [merge_target_pub, pub2] }
+
+          it 'keeps the original DOI of the merge target' do
+            policy.merge!
+            reloaded_target = merge_target_pub.reload
+            expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+            expect(reloaded_target.doi_verified).to be true
+          end
+        end
+
+        context 'when the set of given publications to be merged does not include the merge target' do
+          let(:publications_to_merge) { [pub2] }
+
+          it 'keeps the original DOI of the merge target' do
+            policy.merge!
+            reloaded_target = merge_target_pub.reload
+            expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+            expect(reloaded_target.doi_verified).to be true
+          end
+        end
+
+        context 'when given a second publication to be merged that has a verified DOI that is different than the merge target DOI' do
+          let(:pub3) { create(:publication, doi_verified: true, doi: 'https://doi.org/10.1126/science.ads5951') }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'raises an error' do
+              expect { policy.merge! }.to raise_error DOIVerificationMergePolicy::UnmergablePublications
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'raises an error' do
+              expect { policy.merge! }.to raise_error DOIVerificationMergePolicy::UnmergablePublications
+            end
+          end
+        end
+
+        context 'when given a second publication to be merged that has an unverified DOI' do
+          let(:pub3) { create(:publication, doi_verified: false, doi: 'https://doi.org/10.1126/science.ads5951') }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+        end
+
+        context 'when given a second publication to be merged that has a DOI with unknown verification' do
+          let(:pub3) { create(:publication, doi_verified: nil, doi: 'https://doi.org/10.1126/science.ads5951') }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+        end
+
+        context 'when given a second publication to be merged that has a verified nil DOI' do
+          let(:pub3) { create(:publication, doi_verified: true, doi: nil) }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+        end
+
+        context 'when given a second publication to be merged that has an unverified nil DOI' do
+          let(:pub3) { create(:publication, doi_verified: false, doi: nil) }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+        end
+
+        context 'when given a second publication to be merged that has a nil DOI with unknown verification' do
+          let(:pub3) { create(:publication, doi_verified: nil, doi: nil) }
+
+          context 'when the set of given publications to be merged includes the merge target' do
+            let(:publications_to_merge) { [merge_target_pub, pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+
+          context 'when the set of given publications to be merged does not include the merge target' do
+            let(:publications_to_merge) { [pub2, pub3] }
+
+            it 'keeps the original DOI of the merge target' do
+              policy.merge!
+              reloaded_target = merge_target_pub.reload
+              expect(reloaded_target.doi).to eq 'https://doi.org/10.1103/physrevlett.80.3915'
+              expect(reloaded_target.doi_verified).to be true
+            end
+          end
+        end
+      end
+
       context 'when given a publication to be merged that has a verified DOI that is the same as the merge target DOI' do
         let(:pub2) { create(:publication, doi_verified: true, doi: 'https://doi.org/10.1103/physrevlett.80.3915') }
 
