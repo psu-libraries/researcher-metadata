3caa5c286949342bfc70fae616ef49330e503930
Eric Durante <eric@westarete.com>
2025-02-04 16:24:02 -0500

    Fix indentation

--- spec/component/services/pure_person_finder_spec.rb
+++ spec/component/services/pure_person_finder_spec.rb
@@ -9,17 +9,17 @@ describe PurePersonFinder do
     let(:user1) { instance_double(User, pure_uuid: 'abc123') }
     let(:user2) { instance_double(User, pure_uuid: 'def456') }
 
-      before do
-        allow(HTTParty).to receive(:get).with(
-          'https://pure.psu.edu/ws/api/524/persons/abc123',
-          headers: { 'api-key' => Settings.pure.api_key }
-        ).and_return response1
+    before do
+      allow(HTTParty).to receive(:get).with(
+        'https://pure.psu.edu/ws/api/524/persons/abc123',
+        headers: { 'api-key' => Settings.pure.api_key }
+      ).and_return response1
 
-        allow(HTTParty).to receive(:get).with(
-          'https://pure.psu.edu/ws/api/524/persons/def456',
-          headers: { 'api-key' => Settings.pure.api_key }
-        ).and_return response2
-      end
+      allow(HTTParty).to receive(:get).with(
+        'https://pure.psu.edu/ws/api/524/persons/def456',
+        headers: { 'api-key' => Settings.pure.api_key }
+      ).and_return response2
+    end
 
     context 'when the given publication does not have an author that is present in the Pure dataset' do
       let(:response1) { instance_double HTTParty::Response, code: 404 }
