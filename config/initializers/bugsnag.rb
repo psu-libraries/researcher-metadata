0798ea758e5087f2c9500c5465e7b84411464e5b
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-03-31 12:29:41 -0600

    115 - ignores health checks for bugsnag
    Since RMD also uses OkComputer for health checks, we can use the same logic as in the catalog.

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,4 +3,10 @@
 Bugsnag.configure do |config|
   config.app_version = ENV.fetch('APP_VERSION', nil)
   config.release_stage = ENV.fetch('BUGSNAG_RELEASE_STAGE', 'development')
+
+  config.add_on_error(proc do |event|
+    action = event.request&.dig(:railsAction)
+
+    event.ignore! if action&.start_with?('ok_computer')
+  end)
 end
