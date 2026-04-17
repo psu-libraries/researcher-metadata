546b47a293e7ade736713737974f266af75dca86
karthikreddy-02 <koralla.saikarthi20@gmail.com>
2026-04-17 09:55:05 -0400

    Fix /delayed_job - host not permitted error

--- spec/requests/requests_spec_helper.rb
+++ spec/requests/requests_spec_helper.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 require 'support/database_cleaner'
+require 'support/authentication'
 require 'support/factory_bot'
 require 'support/request_helpers'
 require 'support/fixture'
