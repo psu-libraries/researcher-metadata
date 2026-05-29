32c8c291e17b61e386bcce1373b44dadbb0c7837
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-05-29 15:13:36 -0600

    1227 - remove duplication migration

--- db/migrate/20260529142542_add_draft_scholarsphere_work_deposit_url_to_scholarsphere_work_deposits.rb
+++
@@ -1,5 +0,0 @@
-class AddDraftScholarsphereWorkDepositURLToScholarsphereWorkDeposits < ActiveRecord::Migration[7.2]
-  def change
-    add_column :scholarsphere_work_deposits, :draft_scholarsphere_work_deposit_url, :string
-  end
-end
