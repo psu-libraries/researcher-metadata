86696061053d053033d8d505f030b14c7714202f
Alex Kiessling <ajkiessl@gmail.com>
2026-06-05 15:32:30 -0400

    Refactor

--- app/components/export_metadata_link_component.rb
+++ app/components/export_metadata_link_component.rb
@@ -10,4 +10,13 @@ class ExportMetadataLinkComponent < ViewComponent::Base
   def scholarsphere_export_failed?
     publication.scholarsphere_upload_failed?
   end
+
+  def failed_alert_icon
+    helpers.fa_icon('exclamation-triangle')
+  end
+
+  def failed_message_html
+    I18n.t('components.export_metadata_link_component.failed_message_html',
+           email_link: helpers.mail_to('openaccess@psu.edu', 'openaccess@psu.edu')).html_safe
+  end
 end
